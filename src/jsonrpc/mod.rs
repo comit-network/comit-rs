1c79dd011a9091964d890b8faec58c6629f49359
Thomas Eizinger <thomas@eizinger.io>
2018-04-23 15:19:26 +1000

    Add authorization via default headers

--- src/jsonrpc/mod.rs
+++ src/jsonrpc/mod.rs
@@ -29,21 +29,8 @@ impl JsonRpcClient {
         }
     }
 
-    fn call<T>(&self, id: &str, method: &str, t: T) where T : Serialize {
-        let payload = Payload {
-            jsonrpc: "1.0".to_string(),
-            id: id.to_string(),
-            method: method.to_string(),
-            params: t,
-        };
-
-        let response = self.client
-            .post(self.url.as_str())
-            .json(&payload)
-            .send()
-            .unwrap();
-
-        println!("{:?}", response)
+    pub fn call0(&self, id: &str, method: &str) {
+        self.call::<Vec<i32>>(id, method, vec![])
     }
 
     pub fn call1<A>(&self, id: &str, method: &str, a: A) where A : Serialize {
@@ -53,19 +40,49 @@ impl JsonRpcClient {
     pub fn call2<A, B>(&self, id: &str, method: &str, a: A, b: B) where A : Serialize, B : Serialize {
         self.call(id, method, (a, b))
     }
+
+    fn call<T>(&self, id: &str, method: &str, t: T) where T : Serialize {
+        let payload = Payload {
+            jsonrpc: "1.0".to_string(),
+            id: id.to_string(),
+            method: method.to_string(),
+            params: t,
+        };
+
+        let mut response = self.client
+            .post(self.url.as_str())
+            .json(&payload)
+            .send()
+            .unwrap();
+
+        println!("{:?}", response)
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+    use super::reqwest::header::*;
 
     #[test]
     fn test_adssad() {
-        let client = HTTPClient::new();
-        let rpc_client = JsonRpcClient::new(client, "http://127.0.0.1:8080");
+
+
+        let mut headers = Headers::new();
+        headers.set(Authorization(Basic{
+            username: "bitcoinrpc".to_string(),
+            password: Some("ic1RhcJW+aO3G36iAevasRZA+Q0pOJ5GG9uoGrC0DSpo".to_string())
+        }));
+
+        let client = HTTPClient::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let rpc_client = JsonRpcClient::new(client, "http://127.0.0.1:8332");
 
 //        rpc_client.call1("id", "generate", 100);
-        rpc_client.call2("id", "generate", 100, "adsasd");
+        rpc_client.call0("id", "getblockcount");
     }
 }
