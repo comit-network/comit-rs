28bac06b95417a2698608811969570d97ba8b2fe
Thomas Eizinger <thomas@eizinger.io>
2018-04-23 16:43:14 +1000

    WIP: make lifetimes work

--- src/jsonrpc/mod.rs
+++ src/jsonrpc/mod.rs
@@ -2,11 +2,11 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-use self::reqwest::Client as HTTPClient;
-use self::serde::ser::{Serialize, SerializeSeq, Serializer};
-use self::serde_json::Value;
-use std::collections::HashMap;
+use self::reqwest::{Client as HTTPClient, Error};
+use self::serde::de::Deserialize;
+use self::serde::ser::Serialize;
 use std::string::String;
+use self::serde::Deserializer;
 
 struct JsonRpcClient {
     client: HTTPClient,
@@ -14,11 +14,28 @@ struct JsonRpcClient {
 }
 
 #[derive(Serialize)]
-struct Payload<T> where T : Serialize {
+struct Payload<T> where T: Serialize {
     jsonrpc: String,
     id: String,
     method: String,
     params: T,
+}
+
+#[derive(Debug)]
+struct Response<'a, R: 'a, E: 'a> where R: Deserialize<'a>, E: Deserialize<'a> {
+    id: &'a str,
+    result: &'a R,
+    error: &'a E,
+}
+
+
+impl<'de: 'a, 'a, R, E> Deserialize<'de> for Response<'a, R, E>
+    where R: Deserialize<'a>, E : Deserialize<'a>
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where
+        D: Deserializer<'de> {
+        unimplemented!()
+    }
 }
 
 impl JsonRpcClient {
@@ -29,33 +46,31 @@ impl JsonRpcClient {
         }
     }
 
-    pub fn call0(&self, id: &str, method: &str) {
-        self.call::<Vec<i32>>(id, method, vec![])
+    pub fn call0<'a, E, R>(&self, id: &str, method: &str) -> Result<Response<'a, R, E>, Error> where E: Deserialize<'a>, R: Deserialize<'a> {
+        self.call::<'a, E, R, Vec<i32>>(id, method, vec![])
     }
+//
+//    pub fn call1<'a, E, R, A>(&self, id: &str, method: &str, a: A) -> Result<Response<'a, R, E>, Error> where A: Serialize, E: Deserialize<'a>, R: Deserialize<'a> {
+//        self.call(id, method, [a])
+//    }
+//
+//    pub fn call2<'a, E, R, A, B>(&self, id: &str, method: &str, a: A, b: B) -> Result<Response<'a, R, E>, Error> where A: Serialize, B: Serialize, E: Deserialize<'a>, R: Deserialize<'a> {
+//        self.call(id, method, (a, b))
+//    }
 
-    pub fn call1<A>(&self, id: &str, method: &str, a: A) where A : Serialize {
-        self.call(id, method, [a])
-    }
-
-    pub fn call2<A, B>(&self, id: &str, method: &str, a: A, b: B) where A : Serialize, B : Serialize {
-        self.call(id, method, (a, b))
-    }
-
-    fn call<T>(&self, id: &str, method: &str, t: T) where T : Serialize {
+    fn call<'a, E, R, Params>(&self, id: &str, method: &str, params: Params) -> Result<Response<'a, R, E>, Error> where Params: Serialize, E: Deserialize<'a>, R: Deserialize<'a> {
         let payload = Payload {
             jsonrpc: "1.0".to_string(),
             id: id.to_string(),
             method: method.to_string(),
-            params: t,
+            params,
         };
 
-        let mut response = self.client
+        self.client
             .post(self.url.as_str())
             .json(&payload)
             .send()
-            .unwrap();
-
-        println!("{:?}", response)
+            .and_then(|mut res| res.json::<Response<R, E>>())
     }
 }
 
@@ -67,12 +82,10 @@ mod tests {
 
     #[test]
     fn test_adssad() {
-
-
         let mut headers = Headers::new();
-        headers.set(Authorization(Basic{
+        headers.set(Authorization(Basic {
             username: "bitcoinrpc".to_string(),
-            password: Some("ic1RhcJW+aO3G36iAevasRZA+Q0pOJ5GG9uoGrC0DSpo".to_string())
+            password: Some("ic1RhcJW+aO3G36iAevasRZA+Q0pOJ5GG9uoGrC0DSpo".to_string()),
         }));
 
         let client = HTTPClient::builder()
@@ -83,6 +96,8 @@ mod tests {
         let rpc_client = JsonRpcClient::new(client, "http://127.0.0.1:8332");
 
 //        rpc_client.call1("id", "generate", 100);
-        rpc_client.call0("id", "getblockcount");
+        let result: Result<Response<i32, String>, Error> = rpc_client.call0("id", "getblockcount");
+
+        println!("{:?}", result);
     }
 }
