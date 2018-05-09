9c0d88f49cc6d515e03fbd878609b93f5d9df0a5
Philipp Hoenisch <philipp@hoenisch.at>
2018-05-09 12:04:35 +1000

    Add http client to retrieve exchange rate

--- exchange/src/main.rs
+++ exchange/src/main.rs
@@ -1,3 +1,22 @@
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use reqwest::Error;
+
+#[derive(Debug, Deserialize)]
+struct Rate {
+    sell: String,
+    buy: String,
+    rate: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rate = get();
+    println!("{:?}", rate);
+}
+
+fn get() -> Result<Rate, Error> {
+    reqwest::get("http://localhost:8000/rate/btc/eth")?.json::<Rate>()
 }
