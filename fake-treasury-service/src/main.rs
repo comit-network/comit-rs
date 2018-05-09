ba74f260791150ca196a0977954d25f61c52d4c8
Thomas Eizinger <thomas@eizinger.io>
2018-05-09 11:35:53 +1000

    JSON response for the fake treasury service

--- fake-treasury-service/src/main.rs
+++ fake-treasury-service/src/main.rs
@@ -3,10 +3,26 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+use rocket_contrib::Json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Rate {
+    sell: String,
+    buy: String,
+    rate: f32,
+}
 
 #[get("/<sell>/<buy>")]
-fn rate(sell: String, buy: String) -> String {
-    format!("Hello, {} year old named {}!", sell, buy)
+fn rate(sell: String, buy: String) -> Json<Rate> {
+    Json(Rate {
+        sell,
+        buy,
+        rate: 0.5,
+    })
 }
 
 fn main() {
