d199f12ae7ac0d440270921b15a7a986e9b22f39
LLFourn <lloyd.fourn@gmail.com>
2018-05-11 09:26:07 +1000

    WIP: trading-client offer request
    Still need to parse response and display offer to user

--- trading-client/src/main.rs
+++ trading-client/src/main.rs
@@ -1,9 +1,17 @@
 #[macro_use]
 extern crate structopt;
+
+use std::env::var;
 use std::str::FromStr;
 use structopt::StructOpt;
 
-#[derive(Debug)]
+extern crate reqwest;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Debug, Serialize)]
 struct Currency(String);
 
 impl FromStr for Currency {
@@ -14,7 +22,7 @@ impl FromStr for Currency {
     }
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, Serialize)]
 #[structopt(name = "Trading Client", about = "CLI for the atomic swap trading service.")]
 struct Opt {
     /// The currency you want to sell.
@@ -28,7 +36,52 @@ struct Opt {
     sell_amount: u32,
 }
 
+trait UnwrapOrExit<T, K> {
+    fn unwrap_or_exit(self, msg: &str) -> T;
+}
+
+impl<T, E> UnwrapOrExit<T, E> for Result<T, E> {
+    fn unwrap_or_exit(self, msg: &str) -> T {
+        match self {
+            Ok(success) => success,
+            Err(_) => {
+                eprintln!("{}", msg);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
+// TODO implement API
+// struct TradingServiceClient {
+//     client: reqwest::Client,
+//     service_url: String
+// }
+
+// trait TradingServiceApi {
+//     pub fn create_offer() -> Result<> {
+
+//     }
+// }
+
+// struct FakeTradingServiceClient {
+
+// }
+
 fn main() {
     let opt = Opt::from_args();
+
     println!("{:?}", opt);
+
+    let trading_service_url =
+        var("TRADING_SERVICE_URL").unwrap_or_exit("TRADING_SERVICE_URL not set");
+
+    let client = reqwest::Client::new();
+    let res = client.post(trading_service_url.as_str()).json(&opt).send();
+
+    println!("{:?}", res);
+    // self.client
+    //     .json(&request)
+    //     .send()
+    //     .and_then(|mut res| res.json::<RpcResponse<R>>())
 }
