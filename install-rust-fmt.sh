bb1db206a73c43e8b93e3c9204c365a4d820c457
LLFourn <lloyd.fourn@gmail.com>
2018-05-10 11:05:45 +1000

    Use cat instead echo to write commit hooks
    It's cleaner with a heredoc

--- install-rust-fmt.sh
+++ install-rust-fmt.sh
@@ -6,7 +6,9 @@ rustup component add rustfmt-preview --toolchain=stable-2018-03-29
 touch .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
-echo "#!/bin/bash
+cat > .git/hooks/pre-commit <<'EOF'
+#!/bin/sh
 set -eu
 
-cargo +stable-2018-03-29 fmt -- --write-mode diff" > .git/hooks/pre-commit
+cargo +stable-2018-03-29 fmt -- --write-mode diff
+EOF
