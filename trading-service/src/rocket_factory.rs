23c3a468cfabd2d99d4f8c20fb1463e884e8320f
LLFourn <lloyd.fourn@gmail.com>
2018-05-17 11:04:31 +1000

    Assign OSRng to a variable first
    now that I understand Thomas comment :o

--- trading-service/src/rocket_factory.rs
+++ trading-service/src/rocket_factory.rs
@@ -5,12 +5,11 @@ use std::sync::Mutex;
 use types::{ExchangeApiUrl, Offers};
 
 pub fn create_rocket_instance(exchange_api_url: ExchangeApiUrl, offers: Offers) -> rocket::Rocket {
+    // TODO: allow caller to choose randomness source
+    let rng = OsRng::new().expect("Failed to get randomness from OS");
     rocket::ignite()
         .mount("/", routes![routes::offers::post])
         .manage(exchange_api_url)
         .manage(offers)
-        .manage(Mutex::new(
-            // TODO: allow caller to choose randomness source
-            OsRng::new().expect("Failed to get randomness from OS"),
-        ))
+        .manage(Mutex::new(rng))
 }
