dcd79ce14502c75aa459d4e7716ca197850990f4
LLFourn <lloyd.fourn@gmail.com>
2018-05-24 18:12:00 +1000

    More WIP on EventStore

--- trading_service/src/event_store.rs
+++ trading_service/src/event_store.rs
@@ -1,7 +1,7 @@
 use bitcoin_rpc::Address;
 use exchange_api_client::Offer;
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::RwLock;
 use symbol::Symbol;
 use uuid::Uuid;
 
@@ -33,29 +33,39 @@ impl From<Offer> for OfferCreated {
 }
 
 pub struct EventStore {
-    offer_created_events: Mutex<HashMap<Uuid, OfferCreated>>,
-    trade_created_events: Mutex<HashMap<Uuid, TradeCreated>>,
+    offer_created_events: RwLock<HashMap<Uuid, OfferCreated>>,
+    trade_created_events: RwLock<HashMap<Uuid, TradeCreated>>,
 }
 
 impl EventStore {
     pub fn new() -> EventStore {
         EventStore {
-            offer_created_events: Mutex::new(HashMap::new()),
-            trade_created_events: Mutex::new(HashMap::new()),
+            offer_created_events: RwLock::new(HashMap::new()),
+            trade_created_events: RwLock::new(HashMap::new()),
         }
     }
 
     pub fn store_offer_created(&self, event: OfferCreated) {
-        let mut offers = self.offer_created_events.lock().unwrap();
+        let mut offers = self.offer_created_events.write().unwrap();
         let uid = event.trade_id.clone();
 
         offers.insert(uid, event.clone());
     }
 
     pub fn store_trade_created(&self, event: TradeCreated) {
-        let mut offers = self.trade_created_events.lock().unwrap();
+        let mut offers = self.trade_created_events.write().unwrap();
         let uid = event.trade_id.clone();
 
         offers.insert(uid, event.clone());
+    }
+
+    pub fn get_offer_created(&self, id: &Uuid) -> Option<OfferCreated> {
+        let offers = self.offer_created_events.read().unwrap();
+        offers.get(id).map(|offer| offer.clone())
+    }
+
+    pub fn get_trade_created(&self, id: &Uuid) -> Option<TradeCreated> {
+        let trades = self.trade_created_events.read().unwrap();
+        trades.get(id).map(|trade| trade.clone())
     }
 }
