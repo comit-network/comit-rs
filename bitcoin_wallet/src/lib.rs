13bd954e35fd4cbe88175bf65b394736476fd7ba
LLFourn <lloyd.fourn@gmail.com>
2018-06-04 17:54:11 +1000

    Finish implementation of generate_p2wsh_htlc_redeem_tx and test

--- bitcoin_wallet/src/lib.rs
+++ bitcoin_wallet/src/lib.rs
@@ -2,25 +2,25 @@ extern crate bitcoin;
 extern crate secp256k1;
 #[macro_use]
 extern crate lazy_static;
+extern crate hex;
 
 pub use bitcoin::blockdata::script::Script;
 use bitcoin::blockdata::transaction::Transaction;
 use bitcoin::util::address::Address;
-use bitcoin::util::hash::Sha256dHash;
+use bitcoin::util::hash::Sha256dHash as Txid;
 use bitcoin::util::privkey::Privkey as PrivateKey;
 use secp256k1::Secp256k1;
 
 use bitcoin::blockdata::transaction::TxIn;
 use bitcoin::blockdata::transaction::TxOut;
 use bitcoin::util::bip143::SighashComponents;
+use bitcoin::util::hash::HexError;
 use secp256k1::Message;
 use secp256k1::PublicKey;
 use secp256k1::SecretKey;
 
-//pub use bitcoin::network::serialize:: as serialize_transaction;
-
+#[derive(Debug)]
 pub struct Secret(pub [u8; 32]);
-pub struct Txid(pub [u8; 32]);
 
 lazy_static! {
     static ref SECP: Secp256k1 = Secp256k1::new();
@@ -33,9 +33,10 @@ enum Witness<'a> {
         prev_script: &'a Script,
     },
 }
-
+#[derive(Debug)]
 pub enum Error {
     SECPError(secp256k1::Error),
+    BadHex(HexError),
 }
 
 impl From<secp256k1::Error> for Error {
@@ -44,10 +45,17 @@ impl From<secp256k1::Error> for Error {
     }
 }
 
+impl From<HexError> for Error {
+    fn from(error: HexError) -> Self {
+        Error::BadHex(error)
+    }
+}
+
 fn generate_p2wsh_htlc_redeem_tx(
-    txid: Txid,
+    txid: &Txid,
     vout: u32,
-    amount: u64,
+    input_amount: u64,
+    output_amount: u64,
     htlc_script: &Script,
     secret: &Secret,
     private_key: &PrivateKey,
@@ -56,8 +64,10 @@ fn generate_p2wsh_htlc_redeem_tx(
     let public_key = PublicKey::from_secret_key(&*SECP, &private_key.secret_key())?;
 
     generate_segwit_redeem(
-        Sha256dHash::from_data(&txid.0),
+        txid,
         vout,
+        input_amount,
+        output_amount,
         vec![
             Witness::Signature {
                 private_key: private_key.secret_key(),
@@ -69,19 +79,19 @@ fn generate_p2wsh_htlc_redeem_tx(
             Witness::Data(htlc_script.clone().into_vec()),
         ],
         destination_addr,
-        amount,
     )
 }
 
 fn generate_segwit_redeem(
-    txid: Sha256dHash,
+    txid: &Txid,
     vout: u32,
+    input_amount: u64,
+    output_amount: u64,
     input_witness: Vec<Witness>,
     destination_address: &Address,
-    amount: u64,
 ) -> Result<Transaction, Error> {
     let input = TxIn {
-        prev_hash: txid,
+        prev_hash: txid.clone(),
         prev_index: vout,
         script_sig: Script::new(),
         sequence: 0xFFFFFFFF,
@@ -89,7 +99,7 @@ fn generate_segwit_redeem(
     };
 
     let output = TxOut {
-        value: amount,
+        value: output_amount,
         script_pubkey: destination_address.script_pubkey(),
     };
 
@@ -110,31 +120,29 @@ fn generate_segwit_redeem(
                 prev_script,
             } => {
                 let sighash_components = SighashComponents::new(&transaction);
-                let to_sign = sighash_components.sighash_all(&input, &prev_script, amount);
-                let to_sign = Message::from_slice(&to_sign[..])?;
-                let signature = SECP.sign(&to_sign, &private_key)?;
+                let hash_to_sign =
+                    sighash_components.sighash_all(&input, &prev_script, input_amount);
+                let message_to_sign = Message::from_slice(&hash_to_sign[..])?;
+                let signature = SECP.sign(&message_to_sign, &private_key)?;
                 let mut binary_signature = signature.serialize_der(&*SECP).to_vec();
                 binary_signature.push(1 as u8);
                 witness_data.push(binary_signature);
             }
         }
     }
-    //transaction.input[0].script_sig = Script::from(wrap_push_op(&vec![0xee as u8]));
     transaction.input[0].witness = witness_data;
 
     Ok(transaction)
 }
 
-extern crate bitcoin_rpc;
-extern crate hex;
-
 #[cfg(test)]
 mod tests {
+    extern crate bitcoin_rpc;
 
+    use self::bitcoin_rpc::TxOutConfirmations;
     use super::*;
+    use bitcoin::network::serialize::serialize_hex;
     use bitcoin::util::privkey::Privkey;
-    use bitcoin_rpc;
-    use hex;
     use std::env::var;
     use std::str::FromStr;
 
@@ -143,30 +151,97 @@ mod tests {
         let url = var("BITCOIN_RPC_URL").unwrap();
         let username = var("BITCOIN_RPC_USERNAME").unwrap();
         let password = var("BITCOIN_RPC_PASSWORD").unwrap();
+        let input_amount = 100_000_001;
+
         let private_key =
             Privkey::from_str("cSrWvMrWE3biZinxPZc1hSwMMEdYgYsFpB6iEoh8KraLqYZUUCtt").unwrap();
+
+        let htlc_address = bitcoin_rpc::Address::from(
+            "bcrt1q8msll8hajpvw3ygt9gllx2pkpf0reuyps3x6xelrdk2uzyc77feqy84zm8",
+        );
+
+        let htlc_script = Script::from(hex::decode(
+                "63a82068d627971643a6f97f27c58957826fcba853ec2077fd10ec6b93d8e61deb4cec8876a9142e90d7ea212ad448ea0fa118c7975af9fca9a9956760b27576a914cef2b9c276e2553f86acffaea33a1cb66f1a8a8b6888ac"
+        ).unwrap());
 
         let client =
             bitcoin_rpc::BitcoinCoreClient::new(url.as_str(), username.as_str(), password.as_str());
 
-        client.generate(432); //enable segwit
-        let txid: String = client
-            .send_to_address(
-                bitcoin_rpc::Address::from(
-                    "bcrt1qknf7h6xtuxa9s9yth5x8ejpxue745k85r7sxfz873nkqf7qr8d8qae4dce",
-                ),
-                1.0,
+        client.generate(432).unwrap(); //enable segwit
+
+        let txid = client
+            .send_to_address(htlc_address.clone(), (input_amount as f64) / 100_000_000.0)
+            .unwrap()
+            .into_result()
+            .unwrap();
+
+        client.generate(1).unwrap();
+
+        let raw_htlc_txn = client
+            .get_raw_transaction_serialized(&txid)
+            .unwrap()
+            .into_result()
+            .unwrap();
+
+        let decoded_txn = client
+            .decode_rawtransaction(raw_htlc_txn.clone())
+            .unwrap()
+            .into_result()
+            .unwrap();
+
+        let tx_out = decoded_txn
+            .vout
+            .iter()
+            .find(|txout| txout.matches_address(&htlc_address))
+            .unwrap();
+
+        let alice_rpc_addr = client.get_new_address().unwrap().into_result().unwrap();
+        let alice_addr = alice_rpc_addr.to_bitcoin_address().unwrap();
+
+        let txid_hex: String = txid.into();
+        let txid = Txid::from_hex(txid_hex.as_str()).unwrap();
+
+        let fee = 1000;
+
+        let redeem_tx = generate_p2wsh_htlc_redeem_tx(
+            &txid,
+            tx_out.n,
+            input_amount,
+            input_amount - fee,
+            &htlc_script,
+            &Secret(*b"hello world, you are beautiful!!"),
+            &private_key,
+            &alice_addr,
+        ).unwrap();
+
+        let redeem_tx_hex = serialize_hex(&redeem_tx).unwrap();
+
+        let raw_redeem_tx = bitcoin_rpc::SerializedRawTransaction::from(redeem_tx_hex.as_str());
+
+        let rpc_redeem_txid = client
+            .send_raw_transaction(raw_redeem_tx)
+            .unwrap()
+            .into_result()
+            .unwrap();
+
+        client.generate(1).unwrap();
+
+        let unspent = client
+            .list_unspent(
+                TxOutConfirmations::AtLeast(1),
+                None,
+                Some(vec![alice_rpc_addr]),
             )
             .unwrap()
             .into_result()
-            .unwrap()
-            .into();
+            .unwrap();
 
-        let alice_rpc_addr = client.get_new_address().unwrap().into_result().unwrap();
-
-        let alice_addr = alice_rpc_addr.to_bitcoin_address().unwrap();
-
-        let txid = hex::decode(txid).unwrap();
-        //generate_p2wsh_htlc_redeem_tx()
+        assert!(
+            unspent
+                .iter()
+                .find(|utxo| utxo.txid == rpc_redeem_txid)
+                .is_some(),
+            "utxo should exist after redeeming htlc"
+        );
     }
 }
