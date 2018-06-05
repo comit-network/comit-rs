37438e6bbb8da3774b24c5062d47acc729725415
Thomas Eizinger <thomas@eizinger.io>
2018-06-05 16:12:50 +0800

    Correctly serialize transaction to binary

--- ethereum_wallet/src/transaction.rs
+++ ethereum_wallet/src/transaction.rs
@@ -22,7 +22,12 @@ impl Encodable for Transaction {
         s.append(&self.nonce);
         s.append(&self.gas_price);
         s.append(&self.gas);
-        s.append(&self.to.unwrap_or(Address::new()));
+
+        match self.to {
+            Some(ref address) => s.append(address),
+            None => s.append(&""),
+        };
+
         s.append(&self.value);
         s.append(&self.data.clone().map(|b| b.0).unwrap_or([].to_vec()));
     }
@@ -88,4 +93,50 @@ impl Transaction {
 
         H256(tx_hash)
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn contract_deployment_transaction_should_have_correct_binary_representation() {
+        let tx = Transaction::new_contract_deployment(Bytes(Vec::new()), 500, 2, 10, 1);
+
+        let mut stream = RlpStream::new();
+
+        tx.rlp_append(&mut stream);
+        stream.append(&1u8);
+        stream.append(&0u8);
+        stream.append(&0u8);
+
+        let bytes = stream.as_raw();
+
+        assert_eq!(bytes, &[203, 1, 2, 130, 1, 244, 128, 10, 128, 1, 128, 128]);
+    }
+
+    #[test]
+    fn payment_transaction_should_have_correct_binary_representation() {
+        let tx =
+            Transaction::new_payment("147ba99ef89c152f8004e91999fee87bda6cbc3e", 500, 2, 10, 1);
+
+        let mut stream = RlpStream::new();
+
+        tx.rlp_append(&mut stream);
+        stream.append(&1u8);
+        stream.append(&0u8);
+        stream.append(&0u8);
+
+        let bytes = stream.as_raw();
+
+        assert_eq!(
+            bytes,
+            &[
+                223, 1, 2, 130, 1, 244, 148, 20, 123, 169, 158, 248, 156, 21, 47, 128, 4, 233, 25,
+                153, 254, 232, 123, 218, 108, 188, 62, 10, 128, 1, 128, 128,
+            ]
+        );
+    }
+
 }
