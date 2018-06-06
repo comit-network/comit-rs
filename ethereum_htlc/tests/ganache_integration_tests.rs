a7c65b4ad52ce12e16e1f21322ecebb5d8c2e419
Thomas Eizinger <thomas@eizinger.io>
2018-06-06 09:44:17 +0800

    Use Secret type and generate hash on the fly

--- ethereum_htlc/tests/ganache_integration_tests.rs
+++ ethereum_htlc/tests/ganache_integration_tests.rs
@@ -12,10 +12,12 @@ use web3::types::U256;
 
 mod common;
 use common::GanacheClient;
-use common_types::secret::SecretHash;
+use common_types::secret::{Secret, SecretHash};
 use ethereum_htlc::EpochOffset;
 use std::str::FromStr;
 use web3::types::Address;
+
+const SECRET: &[u8] = b"hello world, you are beautiful!!";
 
 #[test]
 fn given_deployed_htlc_when_redeemed_with_secret_then_money_is_transferred() {
@@ -24,16 +26,13 @@ fn given_deployed_htlc_when_redeemed_with_secret_then_money_is_transferred() {
     let refund_address: Address = "147ba99ef89c152f8004e91999fee87bda6cbc3e".into();
     let success_address: Address = "96984c3e77f38ed01d1c3d98f4bd7c8b11d51d7e".into();
 
-    const SECRET: &[u8] = b"hello world, you are beautiful!!";
-    let secret_hash = SecretHash::from_str(
-        "68d627971643a6f97f27c58957826fcba853ec2077fd10ec6b93d8e61deb4cec",
-    ).unwrap();
+    let mut secret = Secret::new(SECRET.to_vec());
 
     let htlc = ethereum_htlc::Htlc::new(
         EpochOffset::hours(12),
         refund_address,
         success_address,
-        secret_hash,
+        secret.hash(),
     );
 
     let mut client = GanacheClient::new();
@@ -73,15 +72,13 @@ fn given_deployed_htlc_when_refunded_after_timeout_then_money_is_refunded() {
     let refund_address: Address = "03744e31a6b9e6c6f604ff5d8ce1caef1c7bb58c".into();
     let success_address: Address = "25818640c330b071acf5fc836fe0b762a769523d".into();
 
-    let secret_hash = SecretHash::from_str(
-        "68d627971643a6f97f27c58957826fcba853ec2077fd10ec6b93d8e61deb4cec",
-    ).unwrap();
+    let mut secret = Secret::new(SECRET.to_vec());
 
     let htlc = ethereum_htlc::Htlc::new(
         EpochOffset::hours(1),
         refund_address,
         success_address,
-        secret_hash,
+        secret.hash(),
     );
 
     let mut client = GanacheClient::new();
@@ -116,9 +113,7 @@ fn given_deployed_htlc_when_timeout_not_yet_reached_and_wrong_secret_then_nothin
     let refund_address: Address = "f507e0b7cb47e06bb725b605d463a56cef2c057d".into();
     let success_address: Address = "70485b398676fa6c83fa600efd3e63a75e6ac5c2".into();
 
-    let secret_hash = SecretHash::from_str(
-        "68d627971643a6f97f27c58957826fcba853ec2077fd10ec6b93d8e61deb4cec",
-    ).unwrap();
+    let mut secret = Secret::new(SECRET.to_vec());
 
     // FIXME Unfortunately, evm_increaseTime cannot be undone at the moment, so we have to add 2 hours for the increase of the last test.
     // As soon as a version of ganache-cli with ganache-core > v2.1.0 is released (https://github.com/trufflesuite/ganache-core/releases),
@@ -129,7 +124,7 @@ fn given_deployed_htlc_when_timeout_not_yet_reached_and_wrong_secret_then_nothin
         EpochOffset::hours(1 + stupid_offset),
         refund_address,
         success_address,
-        secret_hash,
+        secret.hash(),
     );
 
     let mut client = GanacheClient::new();
