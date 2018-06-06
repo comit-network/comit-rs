ad0479abeb92d20b015beb57d32a8f7422503bf4
Thomas Eizinger <thomas@eizinger.io>
2018-06-06 12:14:37 +0800

    Always run all tests
    This avoids recompilation between runs of the script.

--- run_tests.sh
+++ run_tests.sh
@@ -10,65 +10,18 @@ END(){
 
 trap 'END' EXIT;
 
-requires_bitcoin_rpc="bitcoin_rpc bitcoin_wallet";
-requires_ganache="ethereum_htlc ethereum_wallet ganache_rust_web3";
-
-bitcoin_rpc_integration_tests(){
-    export BITCOIN_RPC_URL="http://localhost:18443"
-    export BITCOIN_RPC_USERNAME="bitcoin"
-    export BITCOIN_RPC_PASSWORD="54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg="
-
-    bitcoin_docker_id="$(sh .docker/bitcoind-regtest)";
-    sleep_for=10
-    echo "sleeping for $sleep_for while bitcoind starts";
-    sleep $sleep_for;
-
-    cmd="cargo test";
-
-    for package in $requires_bitcoin_rpc; do
-        cmd="$cmd --package $package"
-    done
-
-    echo "Executing ${cmd}"
-
-    $cmd;
-}
-
-ganache_integration_tests(){
-
-    export GANACHE_ENDPOINT="http://localhost:8545"
-
-    ganache_docker_id="$(sh .docker/ganache)";
-    sleep_for=10
-    echo "sleeping for $sleep_for while ganache starts";
-    sleep $sleep_for;
-
-    cmd="cargo test";
-
-    for package in $requires_ganache; do
-        cmd="$cmd --package $package"
-    done
-
-    echo "Executing ${cmd}"
-
-    $cmd;
-}
-
-unit_tests(){
-
-    cmd="cargo test --all";
-
-    for package in $requires_bitcoin_rpc $requires_ganache; do
-        cmd="$cmd --exclude $package"
-    done
-
-    echo "Executing ${cmd}"
-
-    $cmd
-}
 
 export RUST_TEST_THREADS=1;
+export BITCOIN_RPC_URL="http://localhost:18443"
+export BITCOIN_RPC_USERNAME="bitcoin"
+export BITCOIN_RPC_PASSWORD="54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg="
+export GANACHE_ENDPOINT="http://localhost:8545"
 
-unit_tests;
-bitcoin_rpc_integration_tests;
-ganache_integration_tests;
+bitcoin_docker_id="$(sh .docker/bitcoind-regtest)";
+ganache_docker_id="$(sh .docker/ganache)";
+
+sleep_for=10
+echo "sleeping for $sleep_for while bitcoind and ganache start";
+sleep $sleep_for;
+
+cargo test --all
