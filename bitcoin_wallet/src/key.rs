724476979f34ed584778f00f5eea422f1fa72c88
LLFourn <lloyd.fourn@gmail.com>
2018-06-19 10:31:21 +1000

    s/ToAddress/ToP2wpkhaddress/
    Renaming allows us to avoid to_address() naming collision with
    rust-bitcoin. There's still a need to make an actual PublicKeyAddress.

--- bitcoin_wallet/src/key.rs
+++ bitcoin_wallet/src/key.rs
@@ -3,21 +3,21 @@ use bitcoin::util::address::Address;
 pub use bitcoin::util::privkey::Privkey as PrivateKey;
 pub use secp256k1::PublicKey;
 
-pub trait ToAddress {
+pub trait ToP2wpkhAddress {
     // note: to_address() is taken by rust-bitcoin so you have call it with
     // ToAddress::to_address()
-    fn to_address(&self, Network) -> Address;
+    fn to_p2wpkh_address(&self, Network) -> Address;
 }
 
-impl ToAddress for PrivateKey {
-    fn to_address(&self, network: Network) -> Address {
-        let secret_pubkey = PublicKey::from_secret_key(&*super::SECP, self.secret_key()).unwrap();
-        secret_pubkey.to_address(network)
+impl ToP2wpkhAddress for PrivateKey {
+    fn to_p2wpkh_address(&self, network: Network) -> Address {
+        let pubkey = PublicKey::from_secret_key(&*super::SECP, self.secret_key()).unwrap();
+        pubkey.to_p2wpkh_address(network)
     }
 }
 
-impl ToAddress for PublicKey {
-    fn to_address(&self, network: Network) -> Address {
+impl ToP2wpkhAddress for PublicKey {
+    fn to_p2wpkh_address(&self, network: Network) -> Address {
         Address::p2wpkh(&self, network)
     }
 }
@@ -31,7 +31,7 @@ mod tests {
         let privkey =
             PrivateKey::from_str("cR6U4gNiCQsPo5gLNP2w6QsLTZkvCGEijhYVPZVhnePQKjMwmas8").unwrap();
 
-        let address = ToAddress::to_address(&privkey, Network::BitcoinCoreRegtest);
+        let address = privkey.to_p2wpkh_address(Network::BitcoinCoreRegtest);
         assert_eq!(
             address,
             Address::from_str("bcrt1qcqslz7lfn34dl096t5uwurff9spen5h4v2pmap").unwrap()
