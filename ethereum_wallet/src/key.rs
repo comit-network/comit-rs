ba830a52d07909980ff67d740bff1cb0e4b6f594
LLFourn <lloyd.fourn@gmail.com>
2018-06-21 13:25:57 +1000

    Extract things in descriptive variables in to_ethereum_address

--- ethereum_wallet/src/key.rs
+++ ethereum_wallet/src/key.rs
@@ -8,13 +8,16 @@ pub trait ToEthereumAddress {
 
 impl ToEthereumAddress for PublicKey {
     fn to_ethereum_address(&self) -> Address {
-        let serialized = self.serialize_uncompressed();
+        let serialized_public_key = self.serialize_uncompressed();
         // Remove the silly openssl 0x04 byte from the front of the
         // serialized public key. This is a bitcoin thing that
         // ethereum doesn't want. Eth pubkey should be 32 + 32 = 64 bytes.
-        let hash = tiny_keccak::keccak256(&serialized[1..]);
+        let actual_public_key = &serialized_public_key[1..];
+        let hash = tiny_keccak::keccak256(actual_public_key);
+        // Ethereum address is the last twenty bytes of the keccak256 hash
+        let ethereum_address_bytes = &hash[12..];
         let mut address = Address::default();
-        address.copy_from_slice(&hash[12..]);
+        address.copy_from_slice(ethereum_address_bytes);
         address
     }
 }
