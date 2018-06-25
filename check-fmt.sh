5cb5eb5d620810d0a71c78fe57903a20ca21be82
Franck Royer <royer.franck@gmail.com>
2018-06-25 02:39:08 +0100

    Use expr

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-CARGO_BIN=$(which cargo) || true
 set -eu
 
-if [ -z "${CARGO_BIN}" ] ; then
-    CARGO_BIN=$HOME/.cargo/bin/cargo
-fi
+CARGO_BIN=$(expr "$(which cargo)" '|' "$HOME/.cargo/bin/cargo");
 
 ${CARGO_BIN} +stable-2018-05-10 fmt -- --write-mode diff
