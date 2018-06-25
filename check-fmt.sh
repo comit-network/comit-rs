79fd58c31134623569e72ddc755cd0c57c74f6c4
Lloyd Fournier <LLFourn@users.noreply.github.com>
2018-06-25 14:55:11 +1000

    Always quote scalar variables in shell scripts

--- check-fmt.sh
+++ check-fmt.sh
@@ -3,4 +3,4 @@ set -eu
 
 CARGO_BIN=$(expr "$(which cargo)" '|' "$HOME/.cargo/bin/cargo");
 
-${CARGO_BIN} +stable-2018-05-10 fmt -- --write-mode diff
+"${CARGO_BIN}" +stable-2018-05-10 fmt -- --write-mode diff
