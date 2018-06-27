bf40f0c241e74831174856c87a40ee58b041d036
Thomas Eizinger <thomas@eizinger.io>
2018-06-27 12:21:32 +0200

    Don't start bitcoind node in run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,9 @@
 set -ev;
 
 END(){
-    if test "${bitcoin_docker_id}${ganache_docker_id}"; then
-        echo "KILLING docker containers $bitcoin_docker_id $ganache_docker_id";
-        docker rm -f $bitcoin_docker_id $ganache_docker_id;
+    if test "${ganache_docker_id}"; then
+        echo "KILLING docker containers $ganache_docker_id";
+        docker rm -f $ganache_docker_id;
     fi
 }
 
@@ -12,17 +12,13 @@ trap 'END' EXIT;
 
 
 export RUST_TEST_THREADS=1;
-export BITCOIN_RPC_URL="http://localhost:18443"
-export BITCOIN_RPC_USERNAME="bitcoin"
-export BITCOIN_RPC_PASSWORD="54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg="
 export GANACHE_ENDPOINT="http://localhost:8545"
 export ETHEREUM_NETWORK_ID=42
 
-bitcoin_docker_id="$(sh .blockchain_nodes/bitcoind-regtest)";
 ganache_docker_id="$(sh .blockchain_nodes/ganache)";
 
 sleep_for=10
-echo "sleeping for $sleep_for while bitcoind and ganache start";
+echo "sleeping for $sleep_for while ganache starts";
 sleep $sleep_for;
 
 cargo test --all
