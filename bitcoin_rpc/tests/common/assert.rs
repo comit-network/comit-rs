ede3c578ce9f62168052ba7ae9ae9e8fdb36c75b
Thomas Eizinger <thomas@eizinger.io>
2018-06-27 10:58:11 +0200

    Kill the docker container after the test

--- bitcoin_rpc/tests/common/assert.rs
+++ bitcoin_rpc/tests/common/assert.rs
@@ -1,19 +1,41 @@
-use self::super::client_factory::create_client;
 use bitcoin_rpc::*;
 use jsonrpc::HTTPError;
 use jsonrpc::RpcError;
 use jsonrpc::RpcResponse;
 use std::fmt::Debug;
+use testcontainers;
+use testcontainers::{Container, Docker, Image, RunArgs};
 
 pub fn assert_successful_result<R, I>(invocation: I)
 where
     R: Debug,
     I: Fn(&BitcoinCoreClient) -> Result<RpcResponse<R>, HTTPError>,
 {
-    let client = create_client();
+    let docker = testcontainers::clients::DockerCli {};
+    let bitcoind = testcontainers::images::Bitcoind::latest();
+
+    let id = docker.run_detached(
+        &bitcoind,
+        RunArgs {
+            ports: bitcoind.exposed_ports(),
+            rm: true,
+            ..RunArgs::default()
+        },
+    );
+    let info = docker.inspect(&id);
+
+    let external_port = info.ports().map_to_external_port(18443).unwrap();
+
+    let url = format!("http://localhost:{}", external_port);
+
+    let username = "bitcoin";
+    let password = "54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg=";
+
+    let client = BitcoinCoreClient::new(url.as_str(), username, password);
+
     let result: Result<R, RpcError> = invocation(&client).unwrap().into();
 
-    let _ = client.stop();
+    docker.rm(&id);
 
     if result.is_err() {
         error!("{:?}", result.unwrap_err());
--- bitcoin_rpc/tests/common/mod.rs
+++ bitcoin_rpc/tests/common/mod.rs
@@ -2,6 +2,5 @@ extern crate bitcoin_rpc;
 extern crate env_logger;
 
 pub mod assert;
-pub mod client_factory;
 pub mod test_client;
 pub mod test_lifecycle;
