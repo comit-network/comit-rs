31f31d35bbf761190b4cd4ddf4db7c80a46f4651
Franck Royer <royer.franck@gmail.com>
2018-07-05 15:06:33 +1000

    Add test to RpcBlockchain

--- bitcoin_rpc/src/types/blockchain.rs
+++ bitcoin_rpc/src/types/blockchain.rs
@@ -21,7 +21,8 @@ pub struct Bip9SoftForkDetails {
     status: String,
     bit: Option<u32>,
     #[serde(rename = "startTime")]
-    start_time: i64, // In regtest, startTime is -1
+    // In regtest, startTime is -1
+    start_time: i64,
     timeout: u64,
     since: u64,
     // TODO: implement before new BIP9
@@ -36,7 +37,7 @@ pub struct Bip9SoftForkDetails {
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
-pub struct Blockchain {
+pub struct RpcBlockchainInfo {
     chain: String,
     blocks: u64,
     headers: u64,
@@ -54,4 +55,124 @@ pub struct Blockchain {
     softforks: Vec<SoftFork>,
     bip9_softforks: Bip9SoftFork,
     warnings: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn can_deserialize_blockchain_response() {
+        let json = r#"{
+        "chain": "regtest",
+        "blocks": 0,
+        "headers": 0,
+        "bestblockhash": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206",
+        "difficulty": 4.65654237390692e-10,
+        "mediantime": 1296688602,
+        "verificationprogress": 1,
+        "initialblockdownload": true,
+        "chainwork": "0000000000000000000000000000000000000000000000000000000000000002",
+        "size_on_disk": 293,
+        "pruned": false,
+        "softforks": [
+            {
+                "id": "bip34",
+                "version": 2,
+                "reject": {
+                    "status": false
+                }
+            },
+            {
+                "id": "bip66",
+                "version": 3,
+                "reject": {
+                    "status": false
+                }
+            },
+            {
+                "id": "bip65",
+                "version": 4,
+                "reject": {
+                    "status": false
+                }
+            }
+        ],
+        "bip9_softforks": {
+            "csv": {
+                "status": "defined",
+                "startTime": 0,
+                "timeout": 9223372036854775807,
+                "since": 0
+            },
+            "segwit": {
+                "status": "active",
+                "startTime": -1,
+                "timeout": 9223372036854775807,
+                "since": 0
+            }
+        },
+        "warnings": ""
+}"#;
+        let blockchain: RpcBlockchainInfo = serde_json::from_str(json).unwrap();
+
+        assert_eq!(
+            blockchain,
+            RpcBlockchainInfo {
+                chain: String::from("regtest"),
+                blocks: 0,
+                headers: 0,
+                bestblockhash: String::from(
+                    "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"
+                ),
+                difficulty: 4.65654237390692e-10,
+                mediantime: 1296688602,
+                verificationprogress: 1.0,
+                initialblockdownload: true,
+                chainwork: String::from(
+                    "0000000000000000000000000000000000000000000000000000000000000002"
+                ),
+                size_on_disk: 293,
+                pruned: false,
+                pruneheight: None,
+                automatic_pruning: None,
+                prune_target_size: None,
+                softforks: vec![
+                    SoftFork {
+                        id: String::from("bip34"),
+                        version: 2,
+                        reject: Reject { status: false },
+                    },
+                    SoftFork {
+                        id: String::from("bip66"),
+                        version: 3,
+                        reject: Reject { status: false },
+                    },
+                    SoftFork {
+                        id: String::from("bip65"),
+                        version: 4,
+                        reject: Reject { status: false },
+                    },
+                ],
+                bip9_softforks: Bip9SoftFork {
+                    csv: Bip9SoftForkDetails {
+                        status: String::from("defined"),
+                        bit: None,
+                        start_time: 0,
+                        timeout: 9223372036854775807,
+                        since: 0,
+                    },
+                    segwit: Bip9SoftForkDetails {
+                        status: String::from("active"),
+                        bit: None,
+                        start_time: -1,
+                        timeout: 9223372036854775807,
+                        since: 0,
+                    },
+                },
+                warnings: String::new(),
+            },
+        )
+    }
 }
