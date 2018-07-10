08fc3e0276c3a4f1d2a37e1a955fa10c696fa778
Franck Royer <royer.franck@gmail.com>
2018-07-10 15:35:50 +1000

    Remove unused deps

--- bitcoin_rpc/src/lib.rs
+++ bitcoin_rpc/src/lib.rs
@@ -1,11 +1,8 @@
-#![feature(try_from)]
-
 extern crate bitcoin;
 extern crate hex as std_hex;
 extern crate jsonrpc;
 #[macro_use]
 extern crate log;
-extern crate regex;
 extern crate rustc_serialize;
 extern crate serde;
 #[macro_use]
