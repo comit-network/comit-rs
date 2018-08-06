1998e46d7cecc621ddd3322b382c156e7c1ceb88
Franck Royer <franck@coblox.tech>
2018-08-06 13:11:25 +1000

    New examples for lightning_rpc
    Few examples to show how to use the lightning_rpc crate
    and access lnd from the Rust library.

---
+++ vendor/lightning_rpc/examples/basic_lnd_calls.rs
@@ -0,0 +1,104 @@
+/// Documentation for `lighting_rpc` examples
+/// To run these examples you need to:
+/// 1. Run lnd. see https://github.com/lightningnetwork/lnd/
+///    We expect lnd to listen on 127.0.0.1:10009
+/// 2. Have access to the lnd's tls.cert:
+///     - By default, it is expected to be at ~/.lnd/tls.cert
+///     - if using docker: `docker cp lnd_btc:/root/.lnd/tls.cert ./`
+/// 3. run this example, passing the path to tls.cert (optional)
+///    `cargo run --package lighting_rpc --example basic_lnd_calls -- $HOME/.lnd/tls.cert`
+extern crate hex;
+extern crate http;
+extern crate lightning_rpc;
+extern crate tower_grpc;
+
+use lightning_rpc::{
+    certificate::{Certificate, FromFile},
+    lightning_rpc_api::LightningRpcApi,
+    lnd_api::LndClient,
+    lnrpc::*,
+};
+use std::env;
+
+static LND_URI: &'static str = "127.0.0.1:10009";
+// This is https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Origin
+static ORIGIN_URI: &'static str = "http://127.0.0.1";
+
+fn main() {
+    let cert_path = env::args().skip(1).next();
+    let cert_path = cert_path.unwrap_or({ format!("{}/.lnd/tls.cert", env::var("HOME").unwrap()) });
+
+    let mut lnd_client = create_lnd_client(cert_path);
+
+    let info = lnd_client.get_info();
+    println!("Lnd Info:\n{:#?}", info.unwrap());
+
+    add_invoice(&mut lnd_client);
+    // do something with ti invoice_response.payment_request.
+    add_invoice_with_pre_image(&mut lnd_client);
+
+    send_payment(&mut lnd_client);
+}
+
+fn add_invoice(lnd_client: &mut LndClient) {
+    let invoice = Invoice {
+        memo: "Example".to_string(),
+        value: 5400,
+        ..Default::default()
+    };
+
+    let response = lnd_client.add_invoice(invoice).unwrap();
+    println!("Payment request: {}", response.payment_request);
+}
+
+// This can only be ran once per LND
+// as LND does not accept 2 invoices with the same image
+fn add_invoice_with_pre_image(lnd_client: &mut LndClient) {
+    let pre_image: Vec<u8> =
+        hex::decode("68d627971643a6f97f27c58957826fcba853ec2077fd10ec6b93d8e61deb4c2c").unwrap();
+
+    let invoice = Invoice {
+        memo: "Test".to_string(),
+        r_preimage: pre_image,
+        value: 5400,
+        ..Default::default()
+    };
+
+    match lnd_client.add_invoice(invoice) {
+        Ok(response) => {
+            let hash = hex::encode(response.r_hash);
+            println!("Hash: {:#?}", hash);
+        }
+        Err(e) => {
+            // If examples are run twice on the same lnd, failure is expected
+            println!("Add Invoice Error: {:?}", e)
+        }
+    };
+}
+
+fn create_lnd_client(cert_path: String) -> LndClient {
+    // Be sure to run the following command once lnd has started:
+    // `docker cp lnd_btc:/root/.lnd/tls.cert ~/src/swap/vendor/lightning_rpc/`
+    let certificate = Certificate::from_file(cert_path).unwrap().into();
+    let lnd_addr = LND_URI.parse().unwrap();
+    let origin_uri: http::Uri = ORIGIN_URI.parse().unwrap();
+    let lnd_client = LndClient::new(certificate, lnd_addr, origin_uri).unwrap();
+    lnd_client
+}
+
+fn send_payment(lnd_client: &mut LndClient) {
+    let payment_request = "lnsb1pdk0tr7pp5gfsjkmqgdgzeadnu7ykxjpsdy2\
+    m7jyrys9zcxeq6kffz9vhucvrqdqvg4uxzmtsd3jscqzysxq97zvuqmcs8396psp7my90d\
+    sq2ws2r34u3fzj6v7rfrlgmdcrrvl6twyt8q2xa9cm6dyd6mr0ppemh6exxjj45smrsl8kgy2uqt667xwwesjtsq9uft9s"
+        .to_string();
+
+    let send_request = SendRequest {
+        payment_request,
+        amt: 5400,
+        ..Default::default()
+    };
+
+    let response = lnd_client.send_payment(send_request);
+    // This will error out as it is a random invoice (cannot find path)
+    println!("Payment response (error) : {:?}", response);
+}
