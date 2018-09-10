41686671174aa8413f061883b69d8c00233a229f
Philipp Hoenisch <philipp@hoenisch.at>
2018-09-10 10:15:33 +1000

    Remove unneeded methods in ApiClient

--- application/comit_node/src/comit_node_api_client/client.rs
+++ application/comit_node/src/comit_node_api_client/client.rs
@@ -38,22 +38,12 @@ pub struct OrderResponseBody<Buy: Ledger, Sell: Ledger> {
 }
 
 pub trait ApiClient: Send + Sync {
-    fn create_buy_offer(
-        &self,
-        symbol: TradingSymbol,
-        amount: f64,
-    ) -> Result<OfferResponseBody<Ethereum, Bitcoin>, reqwest::Error>;
     fn create_buy_order(
         &self,
         symbol: TradingSymbol,
         uid: TradeId,
         trade_request: &OrderRequestBody<Ethereum, Bitcoin>,
     ) -> Result<OrderResponseBody<Ethereum, Bitcoin>, reqwest::Error>;
-    fn create_sell_offer(
-        &self,
-        symbol: TradingSymbol,
-        amount: f64,
-    ) -> Result<OfferResponseBody<Bitcoin, Ethereum>, reqwest::Error>;
     fn create_sell_order(
         &self,
         symbol: TradingSymbol,
@@ -77,20 +67,6 @@ impl DefaultApiClient {
 }
 
 impl ApiClient for DefaultApiClient {
-    fn create_buy_offer(
-        &self,
-        symbol: TradingSymbol,
-        amount: f64,
-    ) -> Result<OfferResponseBody<Ethereum, Bitcoin>, reqwest::Error> {
-        let body = OfferRequestBody { amount };
-
-        self.client
-            .post(format!("{}/trades/{}/buy-offers", self.url.0, symbol).as_str())
-            .json(&body)
-            .send()
-            .and_then(|mut res| res.json::<OfferResponseBody<Ethereum, Bitcoin>>())
-    }
-
     fn create_buy_order(
         &self,
         symbol: TradingSymbol,
@@ -102,20 +78,6 @@ impl ApiClient for DefaultApiClient {
             .json(trade_request)
             .send()
             .and_then(|mut res| res.json::<OrderResponseBody<Ethereum, Bitcoin>>())
-    }
-
-    fn create_sell_offer(
-        &self,
-        symbol: TradingSymbol,
-        amount: f64,
-    ) -> Result<OfferResponseBody<Bitcoin, Ethereum>, reqwest::Error> {
-        let body = OfferRequestBody { amount };
-
-        self.client
-            .post(format!("{}/trades/{}/sell-offers", self.url.0, symbol).as_str())
-            .json(&body)
-            .send()
-            .and_then(|mut res| res.json::<OfferResponseBody<Bitcoin, Ethereum>>())
     }
 
     fn create_sell_order(
