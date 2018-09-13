e5742dd47fc4cebead089102cd3a53539a924961
Thomas Eizinger <thomas@eizinger.io>
2018-09-13 19:03:06 +1000

    Add refund and "inbetween" test case

--- application/comit_node/tests/ganache_cli_erc20_swap_tests.rs
+++ application/comit_node/tests/ganache_cli_erc20_swap_tests.rs
@@ -95,49 +95,179 @@ fn given_deployed_erc20_htlc_when_redeemed_with_secret_then_money_is_transferred
 
     let contract_address = client.get_contract_address(result.unwrap());
 
-    let bob_balance_before = client.get_token_balance(token_contract, bob);
-    assert_eq!(bob_balance_before, U256::from(0));
-
-    let contract_balance = client.get_token_balance(token_contract, contract_address);
-    assert_eq!(contract_balance, U256::from(400));
+    assert_eq!(client.get_token_balance(token_contract, bob), U256::from(0));
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(600)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(400)
+    );
 
     let _ = client.send_data(contract_address, Some(Bytes(SECRET.to_vec())));
 
-    let bob_balance_after = client.get_token_balance(token_contract, bob);
-    assert_eq!(bob_balance_after, U256::from(400));
+    assert_eq!(
+        client.get_token_balance(token_contract, bob),
+        U256::from(400)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(600)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(0)
+    );
 }
-//
-//#[test]
-//fn given_deployed_htlc_when_refunded_after_timeout_then_money_is_refunded() {
-//    let _ = env_logger::try_init();
-//
-//    let refund_address: Address = "03744e31a6b9e6c6f604ff5d8ce1caef1c7bb58c".into();
-//    let success_address: Address = "25818640c330b071acf5fc836fe0b762a769523d".into();
-//
-//    let secret = Secret::from(SECRET.clone());
-//
-//    let htlc = Htlc::new(ONE_HOUR, refund_address, success_address, secret.hash());
-//
-//}
-//
-//#[test]
-//fn given_advanced_timestamp_when_deployed_contract_cannot_yet_be_refunded() {
-//    let _ = env_logger::try_init();
-//
-//    let refund_address: Address = "03744e31a6b9e6c6f604ff5d8ce1caef1c7bb58c".into();
-//    let success_address: Address = "25818640c330b071acf5fc836fe0b762a769523d".into();
-//
-//    let secret = Secret::from(SECRET.clone());
-//
-//}
-//
-//#[test]
-//fn given_deployed_htlc_when_timeout_not_yet_reached_and_wrong_secret_then_nothing_happens() {
-//    let _ = env_logger::try_init();
-//
-//    let refund_address: Address = "f507e0b7cb47e06bb725b605d463a56cef2c057d".into();
-//    let success_address: Address = "70485b398676fa6c83fa600efd3e63a75e6ac5c2".into();
-//
-//    let secret = Secret::from(SECRET.clone());
-//
-//}
+
+#[test]
+fn given_deployed_htlc_when_refunded_after_timeout_then_money_is_refunded() {
+    let _ = env_logger::try_init();
+
+    let container = DockerCli::new().run(ParityEthereum::default());
+    let (_event_loop, web3) = tc_web3_client::new(&container);
+
+    let client = ParityClient::new(web3);
+
+    let token_contract = client.deploy_token_contract();
+
+    let alice_keypair = KeyPair::from_secret_key_hex(
+        "63be4b0d638d44b5fee5b050ab0beeeae7b68cde3d829a3321f8009cdd76b992",
+    ).unwrap();
+    let alice = alice_keypair.public_key().to_ethereum_address();
+
+    let bob_keypair = KeyPair::from_secret_key_hex(
+        "f8218ebf6e2626bd1415c18321496e0c5725f0e1d774c7c2eab69f7650ad6e82",
+    ).unwrap();
+    let bob = bob_keypair.public_key().to_ethereum_address();
+
+    client.mint_1000_tokens(token_contract, alice);
+    client.give_eth_to(alice, EthereumQuantity::from_eth(1.0));
+
+    let service = EthereumService::new(
+        Arc::new(StaticFakeWallet::from_key_pair(alice_keypair.clone())),
+        Arc::new(StaticGasPriceService::default()),
+        Arc::new(tc_web3_client::new(&container)),
+        0,
+    );
+
+    let secret = Secret::from(SECRET.clone());
+
+    let htlc_timeout = Seconds::new(2);
+
+    let htlc_params = Erc20HtlcParams {
+        refund_address: alice,
+        success_address: bob,
+        time_lock: htlc_timeout,
+        amount: U256::from(400),
+        secret_hash: secret.hash(),
+        token_contract_address: token_contract,
+    };
+
+    let result = service.deploy_htlc(htlc_params);
+
+    assert_that(&result).is_ok();
+
+    let contract_address = client.get_contract_address(result.unwrap());
+
+    assert_eq!(client.get_token_balance(token_contract, bob), U256::from(0));
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(600)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(400)
+    );
+
+    // Wait for the contract to expire
+    ::std::thread::sleep(htlc_timeout.into());
+    ::std::thread::sleep(htlc_timeout.into());
+
+    let _ = client.send_data(contract_address, None);
+
+    assert_eq!(client.get_token_balance(token_contract, bob), U256::from(0));
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(1000)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(0)
+    );
+}
+
+#[test]
+fn given_deployed_htlc_when_timeout_not_yet_reached_and_wrong_secret_then_nothing_happens() {
+    let _ = env_logger::try_init();
+
+    let container = DockerCli::new().run(ParityEthereum::default());
+    let (_event_loop, web3) = tc_web3_client::new(&container);
+
+    let client = ParityClient::new(web3);
+
+    let token_contract = client.deploy_token_contract();
+
+    let alice_keypair = KeyPair::from_secret_key_hex(
+        "63be4b0d638d44b5fee5b050ab0beeeae7b68cde3d829a3321f8009cdd76b992",
+    ).unwrap();
+    let alice = alice_keypair.public_key().to_ethereum_address();
+
+    let bob_keypair = KeyPair::from_secret_key_hex(
+        "f8218ebf6e2626bd1415c18321496e0c5725f0e1d774c7c2eab69f7650ad6e82",
+    ).unwrap();
+    let bob = bob_keypair.public_key().to_ethereum_address();
+
+    client.mint_1000_tokens(token_contract, alice);
+    client.give_eth_to(alice, EthereumQuantity::from_eth(1.0));
+
+    let service = EthereumService::new(
+        Arc::new(StaticFakeWallet::from_key_pair(alice_keypair.clone())),
+        Arc::new(StaticGasPriceService::default()),
+        Arc::new(tc_web3_client::new(&container)),
+        0,
+    );
+
+    let secret = Secret::from(SECRET.clone());
+
+    let htlc_timeout = Seconds::new(10);
+
+    let htlc_params = Erc20HtlcParams {
+        refund_address: alice,
+        success_address: bob,
+        time_lock: htlc_timeout,
+        amount: U256::from(400),
+        secret_hash: secret.hash(),
+        token_contract_address: token_contract,
+    };
+
+    let result = service.deploy_htlc(htlc_params);
+
+    assert_that(&result).is_ok();
+
+    let contract_address = client.get_contract_address(result.unwrap());
+
+    assert_eq!(client.get_token_balance(token_contract, bob), U256::from(0));
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(600)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(400)
+    );
+
+    // We don't wait for the contract to expire
+    let _ = client.send_data(contract_address, None);
+
+    assert_eq!(client.get_token_balance(token_contract, bob), U256::from(0));
+    assert_eq!(
+        client.get_token_balance(token_contract, alice),
+        U256::from(600)
+    );
+    assert_eq!(
+        client.get_token_balance(token_contract, contract_address),
+        U256::from(400)
+    );
+}
