c105d2649ad0bc241b87d774c0a5007a85e4c3cc
LLFourn <lloyd.fourn@gmail.com>
2018-09-13 15:12:23 +1000

    Use github web3 everywhere
    Because current 3.1 on crates.io is broken.
    
    And account for the name change of CallResult to CallFuture.

--- vendor/ganache_rust_web3/src/lib.rs
+++ vendor/ganache_rust_web3/src/lib.rs
@@ -3,7 +3,7 @@ extern crate web3;
 extern crate serde_derive;
 extern crate serde_json;
 
-use web3::{api::Namespace, helpers::CallResult, types::U256, Transport};
+use web3::{api::Namespace, helpers::CallFuture, types::U256, Transport};
 
 #[derive(Debug, Clone)]
 pub struct Ganache<T: Transport> {
@@ -15,21 +15,21 @@ pub struct SnapshotId(U256);
 
 impl<T: Transport> Ganache<T> {
     /// Snapshot the state of the blockchain at the current block. Takes no parameters. Returns the integer id of the snapshot created.
-    pub fn evm_snapshot(&self) -> CallResult<SnapshotId, T::Out> {
-        CallResult::new(self.transport.execute("evm_snapshot", vec![]))
+    pub fn evm_snapshot(&self) -> CallFuture<SnapshotId, T::Out> {
+        CallFuture::new(self.transport.execute("evm_snapshot", vec![]))
     }
 
     /// Revert the state of the blockchain to a previous snapshot. Takes a single parameter, which is the snapshot id to revert to. If no snapshot id is passed it will revert to the latest snapshot. Returns true.
-    pub fn evm_revert(&self, id: &SnapshotId) -> CallResult<bool, T::Out> {
+    pub fn evm_revert(&self, id: &SnapshotId) -> CallFuture<bool, T::Out> {
         let id = web3::helpers::serialize(id);
 
-        CallResult::new(self.transport.execute("evm_revert", vec![id]))
+        CallFuture::new(self.transport.execute("evm_revert", vec![id]))
     }
 
     // TODO: This returns some weird crap. Figure out how to hide from the caller.
     /// Jump forward in time. Takes one parameter, which is the amount of time to increase in seconds. Returns the total time adjustment, in seconds.
-    pub fn evm_increase_time(&self, seconds: u64) -> CallResult<u64, T::Out> {
-        CallResult::new(
+    pub fn evm_increase_time(&self, seconds: u64) -> CallFuture<u64, T::Out> {
+        CallFuture::new(
             self.transport
                 .execute("evm_increaseTime", vec![serde_json::Value::from(seconds)]),
         )
@@ -37,8 +37,8 @@ impl<T: Transport> Ganache<T> {
 
     // TODO: This returns "0x0". Figure out how to swallow and hide from the caller.
     /// Force a block to be mined. Takes no parameters. Mines a block independent of whether or not mining is started or stopped.
-    pub fn evm_mine(&self) -> CallResult<String, T::Out> {
-        CallResult::new(self.transport.execute("evm_mine", vec![]))
+    pub fn evm_mine(&self) -> CallFuture<String, T::Out> {
+        CallFuture::new(self.transport.execute("evm_mine", vec![]))
     }
 }
 
