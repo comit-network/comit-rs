b3f3d8f3f0fbf2024f1091e80704c02be67255df
Thomas Eizinger <thomas@eizinger.io>
2018-09-18 12:14:36 +1000

    Remove unused imports

--- application/comit_node/tests/sell_eth_btc.rs
+++ application/comit_node/tests/sell_eth_btc.rs
@@ -20,8 +20,6 @@ extern crate serde_json;
 extern crate uuid;
 #[macro_use]
 extern crate serde_derive;
-extern crate ganache_rust_web3;
-extern crate tc_trufflesuite_ganachecli;
 extern crate tc_web3_client;
 extern crate testcontainers;
 
