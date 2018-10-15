b3bce7b3cd26ea26cde786e2ee0e95f00c7f845b
Lucas Soriano del Pino <lucas.soriano@tenx.tech>
2018-10-15 07:37:46 +0200

    Split comit_node main into functional parts
    - Extract comit server startup into its own function.
    - Clone Arc's before using them as function args, making sure that
      `a.clone()` becomes `Arc::clone(&a)` when `a: Arc`.
    
    It is a Rust convention to use different notation in this case in
    order to acknowledge that it only results in a reference counter
    increase and *not* in a deep copy. Therefore these can be ignored
    when dealing when performance issues.

--- application/comit_node/src/bin/comit_node.rs
+++ application/comit_node/src/bin/comit_node.rs
@@ -29,6 +29,7 @@ use comit_node::{
         alice_ledger_actor::AliceLedgerActor,
         ledger_htlc_service::{BitcoinService, EthereumService},
     },
+    swaps::common::TradeId,
 };
 use ethereum_support::*;
 use event_store::InMemoryEventStore;
@@ -43,16 +44,60 @@ fn main() {
     // TODO: Maybe not print settings because of private keys?
     info!("Starting up with {:#?}", settings);
 
+    //TODO: Integrate all Ethereum keys in this keystore. See #185/#291
+    let key_store = Arc::new(
+        KeyStore::new(settings.bitcoin.extended_private_key)
+            .expect("Could not HD derive keys from the private key"),
+    );
     let event_store = Arc::new(InMemoryEventStore::default());
-    let comit_server_event_store = event_store.clone();
-    let gotham_event_store = event_store.clone();
+    let ethereum_service = create_ethereum_service(&settings);
+    let bitcoin_service = create_bitcoin_service(&settings, Arc::clone(&key_store));
+    let ledger_query_service_api_client = create_ledger_query_service_api_client(&settings);
 
-    let eth_keypair = settings.ethereum.private_key;
+    let mut runtime = tokio::runtime::Runtime::new().unwrap();
 
-    let address = eth_keypair.public_key().to_ethereum_address();
-    let wallet = InMemoryWallet::new(eth_keypair, settings.ethereum.network_id);
+    spawn_gotham_instance(
+        &settings,
+        Arc::clone(&key_store),
+        Arc::clone(&event_store),
+        Arc::clone(&ethereum_service),
+        Arc::clone(&bitcoin_service),
+        Arc::clone(&ledger_query_service_api_client),
+        &mut runtime,
+    );
+    spawn_comit_server(
+        &settings,
+        Arc::clone(&event_store),
+        Arc::clone(&key_store),
+        Arc::clone(&ethereum_service),
+        Arc::clone(&bitcoin_service),
+        Arc::clone(&ledger_query_service_api_client),
+        &mut runtime,
+    );
 
-    let (event_loop, transport) = Http::new(&settings.ethereum.node_url.as_str()).unwrap();
+    // Block the current thread.
+    ::std::thread::park();
+}
+
+fn load_settings() -> ComitNodeSettings {
+    let comit_config_path = var_or_default("COMIT_NODE_CONFIG_PATH", "~/.config/comit_node".into());
+    let run_mode_config = var_or_default("RUN_MODE", "development".into());
+    let default_config = format!("{}/{}", comit_config_path.trim(), "default");
+    let run_mode_config = format!("{}/{}", comit_config_path.trim(), run_mode_config);
+
+    let settings = ComitNodeSettings::new(default_config, run_mode_config);
+    settings.unwrap()
+}
+
+fn create_ethereum_service(settings: &ComitNodeSettings) -> Arc<EthereumService> {
+    let settings = &settings.ethereum;
+
+    let ethereum_keypair = settings.private_key;
+
+    let address = ethereum_keypair.public_key().to_ethereum_address();
+    let wallet = InMemoryWallet::new(ethereum_keypair, settings.network_id);
+
+    let (event_loop, transport) = Http::new(&settings.node_url.as_str()).unwrap();
     let web3 = Web3::new(transport);
 
     let nonce = web3.eth().transaction_count(address, None).wait().unwrap();
@@ -61,34 +106,31 @@ fn main() {
         address, nonce
     );
 
-    let ethereum_service = Arc::new(EthereumService::new(
+    Arc::new(EthereumService::new(
         Arc::new(wallet),
-        Arc::new(StaticGasPriceService::new(settings.ethereum.gas_price)),
+        Arc::new(StaticGasPriceService::new(settings.gas_price)),
         Arc::new((event_loop, web3)),
         nonce,
-    ));
+    ))
+}
 
-    let _eth_refund_address = settings.swap.eth_refund_address;
-
-    let btc_network = settings.bitcoin.network;
-
-    //TODO: Integrate all Ethereum keys in this keystore. See #185/#291
-    let key_store = Arc::new(
-        KeyStore::new(settings.bitcoin.extended_private_key)
-            .expect("Could not HD derive keys from the private key"),
-    );
+fn create_bitcoin_service(
+    settings: &ComitNodeSettings,
+    key_store: Arc<KeyStore>,
+) -> Arc<BitcoinService> {
+    let settings = &settings.bitcoin;
 
     //TODO: make it dynamically generated every X BTC. Could be done with #296
     let btc_bob_redeem_keypair = key_store.get_new_internal_keypair();
     let btc_bob_redeem_address =
-        BitcoinAddress::p2wpkh(btc_bob_redeem_keypair.public_key(), btc_network);
+        BitcoinAddress::p2wpkh(btc_bob_redeem_keypair.public_key(), settings.network);
 
     info!("btc_bob_redeem_address: {}", btc_bob_redeem_address);
 
     let bitcoin_rpc_client = Arc::new(bitcoin_rpc_client::BitcoinCoreClient::new(
-        settings.bitcoin.node_url.as_str(),
-        settings.bitcoin.node_username.as_str(),
-        settings.bitcoin.node_password.as_str(),
+        settings.node_url.as_str(),
+        settings.node_username.as_str(),
+        settings.node_password.as_str(),
     ));
 
     match bitcoin_rpc_client.get_blockchain_info() {
@@ -104,83 +146,88 @@ fn main() {
         Err(e) => error!("Could not connect to Bitcoin RPC because {:?}", e),
     };
 
-    let satoshi_per_kb = settings.bitcoin.satoshi_per_byte;
-    let bitcoin_fee_service = StaticBitcoinFeeService::new(satoshi_per_kb);
-    let bitcoin_fee_service = Arc::new(bitcoin_fee_service);
-    let bitcoin_service = Arc::new(BitcoinService::new(
-        bitcoin_rpc_client.clone(),
-        settings.bitcoin.network,
-        bitcoin_fee_service.clone(),
-        btc_bob_redeem_address.clone(),
-    ));
-    let ledger_query_service = Arc::new(DefaultLedgerQueryServiceApiClient::new(
+    Arc::new(BitcoinService::new(
+        bitcoin_rpc_client,
+        settings.network,
+        Arc::new(StaticBitcoinFeeService::new(settings.satoshi_per_byte)),
+        btc_bob_redeem_address,
+    ))
+}
+
+fn create_ledger_query_service_api_client(
+    settings: &ComitNodeSettings,
+) -> Arc<DefaultLedgerQueryServiceApiClient> {
+    Arc::new(DefaultLedgerQueryServiceApiClient::new(
         &settings.ledger_query_service.url,
-    ));
+    ))
+}
+
+fn spawn_gotham_instance(
+    settings: &ComitNodeSettings,
+    key_store: Arc<KeyStore>,
+    event_store: Arc<InMemoryEventStore<TradeId>>,
+    ethereum_service: Arc<EthereumService>,
+    bitcoin_service: Arc<BitcoinService>,
+    ledger_query_service: Arc<DefaultLedgerQueryServiceApiClient>,
+    runtime: &mut tokio::runtime::Runtime,
+) {
+    let client_pool = comit_client::bam::BamClientPool::default();
+    let remote_comit_node_url = settings.comit.remote_comit_node_url;
 
     let alice_actor = AliceLedgerActor::new(
-        event_store.clone(),
-        ledger_query_service.clone(),
-        bitcoin_service.clone(),
-        btc_network,
-        ethereum_service.clone(),
+        Arc::clone(&event_store),
+        ledger_query_service,
+        bitcoin_service,
+        settings.bitcoin.network,
+        ethereum_service,
         settings.ledger_query_service.bitcoin.poll_interval_secs,
         settings.ledger_query_service.ethereum.poll_interval_secs,
     );
 
-    let mut runtime = tokio::runtime::Runtime::new().unwrap();
     let (alice_actor_sender, alice_actor_future) = alice_actor.listen();
     runtime.spawn(alice_actor_future);
 
-    {
-        let http_api_address_gotham = settings.http_api.address.clone();
-        let http_api_port = settings.http_api.port;
-        let remote_comit_node_url = settings.comit.remote_comit_node_url;
-        let key_store_gotham = key_store.clone();
+    let router = gotham_factory::create_gotham_router(
+        event_store,
+        Arc::new(client_pool),
+        remote_comit_node_url,
+        key_store,
+        alice_actor_sender,
+    );
 
-        let client_pool = comit_client::bam::BamClientPool::default();
+    let http_api_address = settings.http_api.address.clone();
+    let http_api_port = settings.http_api.port;
 
-        std::thread::spawn(move || {
-            let gotham_router = gotham_factory::create_gotham_router(
-                gotham_event_store,
-                Arc::new(client_pool),
-                remote_comit_node_url,
-                key_store_gotham,
-                alice_actor_sender,
-            );
+    gotham::start_on_executor(
+        SocketAddr::from_str(format!("{}:{}", http_api_address, http_api_port).as_str()).unwrap(),
+        router,
+        runtime.executor(),
+    );
+}
 
-            gotham::start(
-                SocketAddr::from_str(
-                    format!("{}:{}", http_api_address_gotham, http_api_port).as_str(),
-                ).unwrap(),
-                gotham_router,
-            );
-        });
-    }
-
+fn spawn_comit_server(
+    settings: &ComitNodeSettings,
+    event_store: Arc<InMemoryEventStore<TradeId>>,
+    key_store: Arc<KeyStore>,
+    ethereum_service: Arc<EthereumService>,
+    bitcoin_service: Arc<BitcoinService>,
+    ledger_query_service: Arc<DefaultLedgerQueryServiceApiClient>,
+    runtime: &mut tokio::runtime::Runtime,
+) {
     let server = ComitServer::new(
-        comit_server_event_store,
-        key_store.clone(),
-        ethereum_service.clone(),
-        bitcoin_service.clone(),
+        event_store,
+        key_store,
+        ethereum_service,
+        bitcoin_service,
         ledger_query_service,
-        btc_network,
+        settings.bitcoin.network,
         settings.ledger_query_service.bitcoin.poll_interval_secs,
         settings.ledger_query_service.ethereum.poll_interval_secs,
     );
 
-    tokio::run(server.listen(settings.comit.comit_listen).map_err(|e| {
+    runtime.spawn(server.listen(settings.comit.comit_listen).map_err(|e| {
         error!("ComitServer shutdown: {:?}", e);
     }));
-}
-
-fn load_settings() -> ComitNodeSettings {
-    let comit_config_path = var_or_default("COMIT_NODE_CONFIG_PATH", "~/.config/comit_node".into());
-    let run_mode_config = var_or_default("RUN_MODE", "development".into());
-    let default_config = format!("{}/{}", comit_config_path.trim(), "default");
-    let run_mode_config = format!("{}/{}", comit_config_path.trim(), run_mode_config);
-
-    let settings = ComitNodeSettings::new(default_config, run_mode_config);
-    settings.unwrap()
 }
 
 fn var_or_default(name: &str, default: String) -> String {
