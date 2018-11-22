2831256532dd9cee6ca1f0990518ca6639287596
Thomas Eizinger <thomas@eizinger.io>
2018-11-22 11:48:38 +1100

    Avoid runtime errors

--- application/comit_node/src/swap_protocols/rfc003/bob/pending_responses.rs
+++ application/comit_node/src/swap_protocols/rfc003/bob/pending_responses.rs
@@ -9,6 +9,10 @@ use swap_protocols::{
     rfc003::{events::ResponseFuture, roles::Bob, state_machine::StateMachineResponse, Ledger},
 };
 
+#[allow(type_alias_bounds)]
+type PendingResponseSender<SL: Ledger, TL: Ledger> =
+    Sender<Result<StateMachineResponse<SL::Identity, TL::Identity, TL::LockDuration>, SwapReject>>;
+
 #[derive(Debug, Default)]
 pub struct PendingResponses<K: Hash + Eq> {
     pending_responses: Mutex<HashMap<K, Box<Any + Send + 'static>>>,
@@ -21,32 +25,30 @@ impl<K: Hash + Eq + Clone + Send + Sync + 'static> PendingResponses<K> {
     ) -> Box<ResponseFuture<Bob<SL, TL, SA, TA>>> {
         let (sender, receiver) = oneshot::channel();
 
-        let mut pending_responses = self.pending_responses.lock().unwrap();
+        let mut pending_responses = match self.pending_responses.lock() {
+            Ok(guard) => guard,
+            Err(e) => e.into_inner(),
+        };
 
         let _old = pending_responses.insert(key, Box::new(sender));
 
         Box::new(receiver.map_err(|_| unreachable!("The sender should never be dropped")))
     }
 
-    pub fn take<SL: Ledger, TL: Ledger>(
-        &self,
-        key: &K,
-    ) -> Option<
-        Sender<
-            Result<StateMachineResponse<SL::Identity, TL::Identity, TL::LockDuration>, SwapReject>,
-        >,
-    > {
-        let mut pending_responses = self.pending_responses.lock().unwrap();
-        pending_responses.remove(key).map(|sender| {
-            let sender = sender
-                .downcast::<Sender<
-                    Result<
-                        StateMachineResponse<SL::Identity, TL::Identity, TL::LockDuration>,
-                        SwapReject,
-                    >,
-                >>()
-                .unwrap();
-            *sender
+    pub fn take<SL: Ledger, TL: Ledger>(&self, key: &K) -> Option<PendingResponseSender<SL, TL>> {
+        let mut pending_responses = match self.pending_responses.lock() {
+            Ok(guard) => guard,
+            Err(e) => e.into_inner(),
+        };
+
+        pending_responses.remove(key).and_then(|sender| {
+            match sender.downcast::<PendingResponseSender<SL, TL>>() {
+                Ok(sender) => Some(*sender),
+                Err(e) => {
+                    error!("Failed to downcast sender to expected type: {:?}", e);
+                    None
+                }
+            }
         })
     }
 }
