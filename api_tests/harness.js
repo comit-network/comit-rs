c65772d45a1463b4bd10bd4cae339ec0943e32ac
Franck Royer <franck@coblox.tech>
2018-12-13 15:41:33 +1100

    Docker compose logs are saved

--- api_tests/harness.js
+++ api_tests/harness.js
@@ -1,4 +1,6 @@
 const execSync = require("child_process").execSync;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const spawn = require("child_process").spawn;
 const Toml = require("toml");
 const fs = require("fs");
@@ -7,6 +9,10 @@ const project_root = execSync("git rev-parse --show-toplevel", {
   encoding: "utf-8"
 }).trim();
 console.log("project_root: ", project_root);
+// FIXME: there is probably a cleaner way to pass this to test_lib.js
+process.env.PROJECT_ROOT = project_root;
+
+const test_lib = require("./test_lib.js"); // needs env var PROJECT_ROOT
 
 const test_dir = process.env.TEST_DIR;
 console.log("test_dir: ", test_dir);
@@ -15,32 +21,37 @@ if (!test_dir) {
   throw new Error("env var $TEST_DIR env variable must be set");
 }
 
+const log_dir = test_dir + "/log";
+console.log("log_dir: ", log_dir);
+// FIXME: there is probably a cleaner way to pass this to test.js
+process.env.LOG_DIR = log_dir;
+
 let pids = [];
-let docker_container_ids = [];
 
 const docker_cwd = project_root + "/api_tests/regtest";
-console.log("docker_cwd: ", docker_cwd);
+
 const docker_compose_options = {
   cwd: docker_cwd,
   encoding: "utf-8"
 };
 
-function end() {
+function cleanUp() {
   pids.forEach(function(pid) {
-    console.log("Killing ", pid);
+    console.log("++ Killing ", pid);
     process.kill(pid);
   });
-
+  console.log("++ Stopping docker containers");
   execSync("docker-compose rm -sfv", docker_compose_options);
+  return;
 }
 
 process.once("SIGINT", function(code) {
-  console.log("SIGINT received...");
-  end();
+  console.log("++ SIGINT received");
+  cleanUp();
 });
 
 const config = Toml.parse(fs.readFileSync(test_dir + "/config.toml", "utf8"));
-console.log("Config: ", config);
+console.log("Config: ", config, "\n\n");
 
 let docker_containers = [];
 Object.keys(config.ledger).forEach(function(ledger) {
@@ -51,12 +62,39 @@ Object.keys(config.ledger).forEach(function(ledger) {
 });
 const docker_container_names = docker_containers.join(" ");
 
-console.log("Starting docker container(s): ", docker_container_names);
-execSync("docker-compose up -d " + docker_container_names, docker_compose_options);
-spawn("docker-compose logs --tail=all", {
-  cwd: docker_cwd,
-  encoding: "utf-8",
-  stdio: ["ignore", fs.openSync("std.out", "w"), fs.openSync("std.err", "w")]
-});
+describe("Harness", async function() {
 
-end();
+  before(async function() {
+    this.timeout(50000);
+
+    console.log("++ Starting docker container(s): ", docker_container_names);
+    await execSync(
+      "docker-compose up -d " + docker_container_names,
+      docker_compose_options
+    );
+    console.log("++ Docker containers started")
+
+    spawn("docker-compose", ["logs", "--tail=all", "-f"], {
+      cwd: docker_cwd,
+      encoding: "utf-8",
+      stdio: [
+        "ignore",
+        fs.openSync(log_dir + "/docker-compose.log", "w"),
+        fs.openSync(log_dir + "/docker-compose-err.log", "w")
+      ]
+    });
+
+    console.log("Start sleeping");
+    await test_lib.sleep(10000);
+    console.log("Well rested");
+  });
+
+  it("This is my test", async () => {
+    return;
+  });
+
+  after(async function() {
+    return cleanUp();
+  });
+
+});
