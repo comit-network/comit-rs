a6f23486249c2d861b37a4fdfa7f278e083395db
Franck Royer <franck@coblox.tech>
2019-03-26 15:29:58 +1100

    Debug derive is not needed, nor is the macro_use

--- application/comit_node/src/swap_protocols/rfc003/ledger_state.rs
+++ application/comit_node/src/swap_protocols/rfc003/ledger_state.rs
@@ -1,7 +1,7 @@
 use crate::swap_protocols::rfc003::ledger::Ledger;
 
 #[derive(Clone, Debug, PartialEq, EnumDiscriminants)]
-#[strum_discriminants(name(HtlcState), derive(Debug, Serialize))]
+#[strum_discriminants(name(HtlcState), derive(Serialize))]
 pub enum LedgerState<L: Ledger> {
     NotDeployed,
     Deployed {
