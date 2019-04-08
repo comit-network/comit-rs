8d1c1a66442daa7c9a4ce5e01acd0c0108e40254
Franck Royer <franck@coblox.tech>
2019-04-08 22:01:14 +1000

    Download and unpack bundle

---
+++ vendor/comit_i/build.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+
+fn main() -> Result<(), std::io::Error> {
+    let version = include_str!("./version").trim();
+
+    let url = format!(
+        "https://github.com/comit-network/comit-i/releases/download/{}/bundle.zip",
+        version
+    );
+
+    let mut tmpfile: File = tempfile::tempfile().unwrap();
+    reqwest::get(&url).unwrap().copy_to(&mut tmpfile).unwrap();
+    unzip::Unzipper::new(tmpfile, "./").unzip().unwrap();
+
+    println!("cargo:rerun-if-changed={}", "./version");
+
+    Ok(())
+}
