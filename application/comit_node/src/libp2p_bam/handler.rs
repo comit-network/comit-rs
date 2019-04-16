58933adbe97c1bc6e26aabf8a53226b6cf2384e3
Thomas Eizinger <thomas@eizinger.io>
2019-04-16 16:10:56 +1000

    Simplify libp2p protocol handler

--- application/comit_node/src/libp2p_bam/handler.rs
+++ application/comit_node/src/libp2p_bam/handler.rs
@@ -23,50 +23,45 @@ use libp2p::core::{
 use std::{
     collections::{vec_deque::VecDeque, HashMap, HashSet},
     convert::Infallible,
-    marker::PhantomData,
+    mem,
 };
 use tokio::{
     codec::Framed,
     prelude::{AsyncRead, AsyncWrite},
 };
 
+enum SocketState<TSubstream> {
+    Disconnected,
+    Requested,
+    Waiting(Task),
+    Connected(Framed<Negotiated<TSubstream>, JsonFrameCodec>),
+}
+
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct BamHandler<TSubstream> {
-    marker: PhantomData<TSubstream>,
+    known_headers: HashMap<String, HashSet<String>>,
     #[derivative(Debug = "ignore")]
-    framed: Option<Framed<Negotiated<TSubstream>, JsonFrameCodec>>,
+    network_socket: SocketState<TSubstream>,
+    pending_frames: VecDeque<Frame>,
 
     next_outgoing_id: u32,
     pending_outgoing_request_channels: HashMap<u32, oneshot::Sender<Response>>,
 
     next_incoming_id: u32,
-
-    #[derivative(Debug = "ignore")]
-    pending_incoming_request_channels:
-        HashMap<u32, Box<dyn Future<Item = Response, Error = ()> + Send>>,
-
-    pending_frames: VecDeque<Frame>,
-
-    known_headers: HashMap<String, HashSet<String>>,
-
-    current_task: Option<Task>,
-    waiting_for_outbound_stream_task: Option<Task>,
+    pending_incoming_request_channels: HashMap<u32, oneshot::Receiver<Response>>,
 }
 
 impl<TSubstream> BamHandler<TSubstream> {
     pub fn new(known_headers: HashMap<String, HashSet<String>>) -> Self {
         Self {
-            marker: PhantomData,
-            framed: None,
+            known_headers,
+            pending_frames: VecDeque::new(),
+            network_socket: SocketState::Disconnected,
             next_outgoing_id: 0,
             pending_outgoing_request_channels: HashMap::new(),
             next_incoming_id: 0,
             pending_incoming_request_channels: HashMap::new(),
-            pending_frames: VecDeque::new(),
-            known_headers,
-            current_task: None,
-            waiting_for_outbound_stream_task: None,
         }
     }
 }
@@ -100,11 +95,10 @@ impl<TSubstream: AsyncRead + AsyncWrite> ProtocolsHandler for BamHandler<TSubstr
         &mut self,
         protocol: Framed<Negotiated<TSubstream>, JsonFrameCodec>,
     ) {
-        self.framed = Some(protocol);
+        let current_state =
+            mem::replace(&mut self.network_socket, SocketState::Connected(protocol));
 
-        log::debug!("received fully negotiated connection!");
-
-        if let Some(task) = &self.current_task {
+        if let SocketState::Waiting(task) = current_state {
             task.notify();
         }
     }
@@ -114,18 +108,15 @@ impl<TSubstream: AsyncRead + AsyncWrite> ProtocolsHandler for BamHandler<TSubstr
         protocol: Framed<Negotiated<TSubstream>, JsonFrameCodec>,
         _info: Self::OutboundOpenInfo,
     ) {
-        self.framed = Some(protocol);
+        let current_state =
+            mem::replace(&mut self.network_socket, SocketState::Connected(protocol));
 
-        log::debug!("received fully negotiated connection!");
-
-        if let Some(task) = &self.waiting_for_outbound_stream_task {
+        if let SocketState::Waiting(task) = current_state {
             task.notify();
         }
     }
 
     fn inject_event(&mut self, event: Self::InEvent) {
-        log::debug!("received event: {:?}", event.request);
-
         let PendingOutgoingRequest { request, channel } = event;
 
         let frame = request.into_frame(self.next_outgoing_id);
@@ -135,10 +126,6 @@ impl<TSubstream: AsyncRead + AsyncWrite> ProtocolsHandler for BamHandler<TSubstr
             .insert(self.next_outgoing_id, channel);
 
         self.next_outgoing_id += 1;
-
-        if let Some(task) = &self.current_task {
-            task.notify();
-        }
     }
 
     fn inject_dial_upgrade_error(
@@ -160,113 +147,43 @@ impl<TSubstream: AsyncRead + AsyncWrite> ProtocolsHandler for BamHandler<TSubstr
         >,
         Self::Error,
     > {
-        log::debug!("polling handler.");
-        log::debug!(
-            "{} pending requests",
-            self.pending_incoming_request_channels.len()
-        );
-        log::debug!("{} pending frames", self.pending_frames.len());
-
         for (id, future) in self.pending_incoming_request_channels.iter_mut() {
             match future.poll() {
                 Ok(Async::Ready(response)) => {
                     let frame = response.into_frame(*id);
-
                     self.pending_frames.push_back(frame);
-
-                    log::debug!("response for request {} is ready, created new frame", id)
                 }
-                Ok(Async::NotReady) => {
-                    // FIXME: should we really early return here?
-                    return Ok(Async::NotReady);
-                }
-                Err(_) => log::warn!("error while polling response for request {}", id),
+                Ok(Async::NotReady) => return Ok(Async::NotReady),
+                Err(e) => log::warn!("error while polling response for request {}: {:?}", id, e),
             }
         }
 
-        match self.framed.as_mut() {
-            Some(framed) => {
-                match framed.poll_complete() {
-                    Ok(Async::Ready(_)) => log::debug!("sink successfully flushed!"),
-                    Ok(Async::NotReady) => {
-                        log::debug!("more items to be sent over the sink");
-                        return Ok(Async::NotReady);
-                    }
-                    Err(e) => return Err(e),
-                }
+        match &mut self.network_socket {
+            SocketState::Connected(socket) => {
+                futures::try_ready!(socket.poll_complete());
 
-                if let Some(frame) = self.pending_frames.pop_front() {
-                    match framed.start_send(frame) {
-                        Ok(AsyncSink::Ready) => {
-                            // cool, we sent it!
-
-                            match framed.poll_complete() {
-                                Ok(Async::Ready(_)) => log::debug!("sink successfully flushed!"),
-                                Ok(Async::NotReady) => {
-                                    log::debug!("more items to be sent over the sink");
-                                    return Ok(Async::NotReady);
-                                }
-                                Err(e) => return Err(e),
-                            }
-                        }
+                while let Some(frame) = self.pending_frames.pop_front() {
+                    match socket.start_send(frame) {
+                        Ok(AsyncSink::Ready) => futures::try_ready!(socket.poll_complete()),
                         Ok(AsyncSink::NotReady(pending_frame)) => {
                             self.pending_frames.push_front(pending_frame);
-
-                            log::debug!("sink is full, returning Async::NotReady");
-
                             return Ok(Async::NotReady);
                         }
                         Err(e) => return Err(e),
                     }
                 }
 
-                match framed.poll() {
-                    Ok(Async::Ready(Some(frame))) => {
-                        let id = frame.id;
-
-                        let option = match frame.frame_type.as_str() {
-                            "REQUEST" => self.handle_request(frame),
-                            "RESPONSE" => {
-                                self.handle_response(frame);
-                                None
-                            }
-                            _ => {
-                                self.handle_unknown_frame(frame);
-                                None
-                            }
-                        };
-
-                        match option {
-                            Some(request) => {
-                                log::debug!("got new incoming request, emitting event");
-                                return Ok(Async::Ready(ProtocolsHandlerEvent::Custom(request)));
-                            }
-                            None => {
-                                // TODO: this is probably wrong
-                                self.current_task = Some(task::current());
-
-                                log::debug!("handling frame {} yielded no element, returning Async::NotReady", id);
-
-                                return Ok(Async::NotReady);
-                            }
+                match futures::try_ready!(socket.poll()) {
+                    Some(frame) => {
+                        if let Some(request) = self.handle_frame(frame) {
+                            return Ok(Async::Ready(ProtocolsHandlerEvent::Custom(request)));
                         }
-                    }
-                    Ok(Async::Ready(None)) => unimplemented!("TBD"),
-                    Ok(Async::NotReady) => return Ok(Async::NotReady),
-                    Err(e) => return Err(e),
-                }
-            }
-            None => {
-                log::debug!("connection to remote not yet fully established, cannot do any work");
-
-                match self.waiting_for_outbound_stream_task {
-                    Some(_) => {
-                        self.waiting_for_outbound_stream_task = Some(task::current());
 
                         return Ok(Async::NotReady);
                     }
                     None => {
-                        self.waiting_for_outbound_stream_task = Some(task::current());
+                        log::info!("substream is closed, trying to reconnect");
+                        self.network_socket = SocketState::Requested;
 
                         return Ok(Async::Ready(
                             ProtocolsHandlerEvent::OutboundSubstreamRequest {
@@ -277,11 +194,40 @@ impl<TSubstream: AsyncRead + AsyncWrite> ProtocolsHandler for BamHandler<TSubstr
                     }
                 }
             }
+            SocketState::Disconnected => {
+                self.network_socket = SocketState::Requested;
+
+                return Ok(Async::Ready(
+                    ProtocolsHandlerEvent::OutboundSubstreamRequest {
+                        upgrade: BamConfig {},
+                        info: (),
+                    },
+                ));
+            }
+            SocketState::Requested | SocketState::Waiting(_) => {
+                self.network_socket = SocketState::Waiting(task::current());
+
+                return Ok(Async::NotReady);
+            }
         }
     }
 }
 
 impl<TSubstream> BamHandler<TSubstream> {
+    fn handle_frame(&mut self, frame: Frame) -> Option<PendingIncomingRequest> {
+        match frame.frame_type.as_str() {
+            "REQUEST" => self.handle_request(frame),
+            "RESPONSE" => {
+                self.handle_response(frame);
+                None
+            }
+            _ => {
+                self.handle_unknown_frame(frame);
+                None
+            }
+        }
+    }
+
     fn handle_request(&mut self, frame: Frame) -> Option<PendingIncomingRequest> {
         if frame.id < self.next_incoming_id {
             log::warn!(
@@ -302,14 +248,8 @@ impl<TSubstream> BamHandler<TSubstream> {
             Ok(validated_incoming_request) => {
                 let (sender, receiver) = oneshot::channel();
 
-                let channel = Box::new(receiver.map_err(|_| {
-                    log::warn!(
-                        "Sender of response future was unexpectedly dropped before response was received."
-                    )
-                }));
-
                 self.pending_incoming_request_channels
-                    .insert(frame.id, channel);
+                    .insert(frame.id, receiver);
 
                 return Some(PendingIncomingRequest {
                     request: validated_incoming_request,
@@ -332,10 +272,7 @@ impl<TSubstream> BamHandler<TSubstream> {
 
         match maybe_request_channel {
             Some(sender) => match response {
-                Ok(response) => {
-                    log::debug!("dispatching {:?} to stored handler", response);
-                    sender.send(response).unwrap()
-                }
+                Ok(response) => sender.send(response).unwrap(),
                 Err(e) => log::error!(
                     "payload of frame {} is not a well-formed RESPONSE: {:?}",
                     frame.id,
