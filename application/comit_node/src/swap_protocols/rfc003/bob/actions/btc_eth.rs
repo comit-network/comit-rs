90ceb7b59b122c32f8346dd67e30f109d3cc10e8
Philipp Hoenisch <philipp@hoenisch.at>
2019-04-23 10:39:42 +1000

    Reuse some actions in btc_eth
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

--- application/comit_node/src/swap_protocols/rfc003/bob/actions/btc_eth.rs
+++ application/comit_node/src/swap_protocols/rfc003/bob/actions/btc_eth.rs
@@ -1,68 +1,21 @@
 use crate::swap_protocols::{
     ledger::{Bitcoin, Ethereum},
     rfc003::{
-        self, bitcoin,
+        actions::CreateActions,
+        bitcoin,
         bob::{
             self,
             actions::{Accept, Decline},
             SwapCommunication,
         },
-        ethereum::{self, EtherHtlc},
-        secret::Secret,
-        secret_source::SecretSource,
+        ethereum,
         state_machine::HtlcParams,
         Action, Actions, LedgerState,
     },
 };
-use bitcoin_support::{BitcoinQuantity, OutPoint};
-use bitcoin_witness::PrimedInput;
-use ethereum_support::{Bytes, EtherQuantity};
+use bitcoin_support::BitcoinQuantity;
+use ethereum_support::EtherQuantity;
 use std::sync::Arc;
-
-type Request = rfc003::messages::Request<Bitcoin, Ethereum, BitcoinQuantity, EtherQuantity>;
-type Response = rfc003::messages::AcceptResponseBody<Bitcoin, Ethereum>;
-
-fn fund_action(request: &Request, response: &Response) -> ethereum::ContractDeploy {
-    HtlcParams::new_beta_params(request, response).into()
-}
-
-fn refund_action(
-    request: &Request,
-    beta_htlc_location: ethereum_support::Address,
-) -> ethereum::SendTransaction {
-    let data = Bytes::default();
-    let gas_limit = EtherHtlc::tx_gas_limit();
-    let network = request.beta_ledger.network;
-
-    ethereum::SendTransaction {
-        to: beta_htlc_location,
-        data,
-        gas_limit,
-        amount: EtherQuantity::zero(),
-        network,
-    }
-}
-
-fn redeem_action(
-    request: &Request,
-    response: &Response,
-    alpha_htlc_location: OutPoint,
-    secret_source: &dyn SecretSource,
-    secret: Secret,
-) -> bitcoin::SpendOutput {
-    let alpha_asset = request.alpha_asset;
-    let htlc = bitcoin::Htlc::from(HtlcParams::new_alpha_params(request, response));
-    let network = request.alpha_ledger.network;
-
-    bitcoin::SpendOutput {
-        output: PrimedInput::new(
-            alpha_htlc_location,
-            alpha_asset,
-            htlc.unlock_with_secret(secret_source.secp256k1_redeem(), &secret),
-        ),
-        network,
-    }
-}
 
 impl Actions for bob::State<Bitcoin, Ethereum, BitcoinQuantity, EtherQuantity> {
     type ActionKind = bob::ActionKind<
@@ -101,27 +54,36 @@ impl Actions for bob::State<Bitcoin, Ethereum, BitcoinQuantity, EtherQuantity> {
 
         use self::LedgerState::*;
         let mut actions = match (alpha_state, beta_state, self.secret) {
-            (Funded { htlc_location, .. }, _, Some(secret)) => {
-                vec![bob::ActionKind::Redeem(redeem_action(
-                    &request,
-                    &response,
-                    *htlc_location,
-                    self.secret_source.as_ref(),
-                    secret,
-                ))
-                .into_action()]
+            (Funded { htlc_location, .. }, _, Some(_secret)) => {
+                vec![
+                    bob::ActionKind::Redeem(<(Bitcoin, BitcoinQuantity)>::redeem_action(
+                        HtlcParams::new_alpha_params(request, response),
+                        htlc_location.clone(),
+                        &*self.secret_source,
+                    ))
+                    .into_action(),
+                ]
             }
             (Funded { .. }, NotDeployed, _) => {
-                vec![bob::ActionKind::Fund(fund_action(&request, &response)).into_action()]
+                vec![
+                    bob::ActionKind::Fund(<(Ethereum, EtherQuantity)>::fund_action(
+                        HtlcParams::new_beta_params(request, response),
+                    ))
+                    .into_action(),
+                ]
             }
             _ => vec![],
         };
 
         if let Funded { htlc_location, .. } = beta_state {
             actions.push(
-                bob::ActionKind::Refund(refund_action(&request, *htlc_location))
-                    .into_action()
-                    .with_invalid_until(request.beta_expiry),
+                bob::ActionKind::Refund(<(Ethereum, EtherQuantity)>::refund_action(
+                    HtlcParams::new_beta_params(request, response),
+                    htlc_location.clone(),
+                    &*self.secret_source,
+                ))
+                .into_action()
+                .with_invalid_until(request.beta_expiry),
             )
         }
 
