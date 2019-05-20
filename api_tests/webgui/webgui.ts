4ca4da981491c3a228d97c3ddec176e775950370
Franck Royer <franck@coblox.tech>
2019-05-20 10:11:59 +1000

    Serve comit_node connection details
    Serve comit node http api port/address on same endpoint than comit-i to allow comit-i to use it.
    
    Resolves #945

--- api_tests/webgui/webgui.ts
+++ api_tests/webgui/webgui.ts
@@ -2,10 +2,11 @@
 // They are system tests that checks that comit-i can be accessed and that comit-i can connect to comit_node
 // Functional tests on the GUI should only be ran in the comit-i repo!
 import { Actor } from "../lib/actor";
-import { use, request } from "chai";
+import { use, request, expect } from "chai";
 import { HarnessGlobal } from "../lib/util";
 import "chai/register-should";
 import chaiHttp = require("chai-http");
+const requestPromise = require("request-promise-native");
 
 use(chaiHttp);
 
@@ -74,6 +75,26 @@ setTimeout(async function() {
             let res = await request(david.web_gui_url()).get("/");
 
             res.should.have.status(200);
+        });
+
+        it("[David] returns comit_node http api settings on /config/comit_node.js for GET", async function() {
+            const res = await requestPromise.get(
+                david.web_gui_url() +
+                    "/config/comitNode.js?callback=callbackFunctionName",
+                {
+                    resolveWithFullResponse: true,
+                }
+            );
+
+            res.should.have.property("statusCode", 200);
+            res.should.have.property("body");
+            const body: string = res.body;
+            expect(body).to.match(/^function callbackFunctionName/);
+
+            let fn = eval("(" + res.body + ")");
+            let connDetails = fn();
+            expect(connDetails).to.have.property("host", "127.0.0.1");
+            expect(connDetails).to.have.property("port", 8000);
         });
     });
 
