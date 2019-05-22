47e4efa770347f6d720d7574df43f672a9c8e471
Philipp Hoenisch <philipp@hoenisch.at>
2019-05-22 11:29:25 +1000

    Delete test files after they were needed
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

--- application/comit_node/src/settings/mod.rs
+++ application/comit_node/src/settings/mod.rs
@@ -210,12 +210,23 @@ mod tests {
         let default_settings =
             ComitNodeSettings::create_with_default(config_path.clone(), config_file.clone());
         let settings = ComitNodeSettings::read(config_path.join(config_file));
+        delete_tmp_files(&config_path, config_file);
 
         assert_that(&default_settings).is_ok();
         assert_that(&settings).is_ok();
         assert_that(&default_settings.unwrap()).is_equal_to(&settings.unwrap());
 
         delete_tmp_files(&config_path, config_file);
+    }
+
+    fn delete_tmp_files(config_path: &PathBuf, config_file: &str) {
+        if config_path.exists() {
+            if config_path.clone().join(config_file).exists() {
+                let default_config_file = config_path.clone().join(config_file);
+                fs::remove_file(default_config_file).unwrap();
+            }
+            fs::remove_dir(config_path.clone()).unwrap();
+        }
     }
 
     fn delete_tmp_files(config_path: &PathBuf, config_file: &str) {
