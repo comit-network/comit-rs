a082ee0179686eedc1dba4c3e01a423da858e112
Philipp Hoenisch <philipp@hoenisch.at>
2019-05-28 14:28:07 +1000

    Have fallback config file location for btsieve
    Signed-off-by: Philipp Hoenisch <philipp@hoenisch.at>

--- application/btsieve/src/load_settings.rs
+++ application/btsieve/src/load_settings.rs
@@ -1,22 +1,43 @@
 use crate::settings::Settings;
 use config::ConfigError;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 pub struct Opt {
     /// Path to configuration folder
     #[structopt(short = "c", long = "config", parse(from_os_str))]
-    config_file: PathBuf,
+    config_file: Option<PathBuf>,
 }
 
 pub fn load_settings(opt: Opt) -> Result<Settings, ConfigError> {
-    if opt.config_file.exists() {
-        Settings::read(opt.config_file)
-    } else {
-        Err(ConfigError::Message(format!(
-            "Could not load config file: {:?}",
-            opt.config_file
-        )))
+    match opt.config_file {
+        Some(config_file) => {
+            if config_file.exists() {
+                Settings::read(config_file)
+            } else {
+                Err(ConfigError::Message(format!(
+                    "Could not load config file: {:?}",
+                    config_file
+                )))
+            }
+        }
+        None => match directories::UserDirs::new() {
+            None => Err(ConfigError::Message(
+                "Unable to determine user's home directory".to_string(),
+            )),
+            Some(dirs) => {
+                let config_file = Path::join(dirs.home_dir(), ".config/comit/btsieve.toml");
+                log::info!("Config file was not provided - looking up config file in default location at: {:?}", config_file);;
+                if config_file.exists() {
+                    Settings::read(config_file)
+                } else {
+                    Err(ConfigError::Message(format!(
+                        "Could not load config file: {:?}",
+                        config_file
+                    )))
+                }
+            }
+        },
     }
 }
