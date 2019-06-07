c0128bd07dfbc990db142cde8faec64bea9527ce
Thomas Eizinger <thomas@eizinger.io>
2019-06-07 19:15:15 +1000

    Rewrite PeerID + IP tests
    Given the new functionality, we can now actually test the originally
    desired behaviour: Not connecting to a node if the IP does not resolve
    to the given PeerID.

--- api_tests/dry/peers_using_ip.ts
+++ api_tests/dry/peers_using_ip.ts
@@ -1,6 +1,6 @@
 import { Actor } from "../lib/actor";
 import { HarnessGlobal, sleep } from "../lib/util";
-import { request, expect } from "chai";
+import { expect, request } from "chai";
 import { toWei } from "web3-utils";
 import "../lib/setupChai";
 
@@ -11,7 +11,6 @@ declare var global: HarnessGlobal;
     const bob = new Actor("bob", global.config, global.project_root);
     const charlie = new Actor("charlie", global.config, global.project_root);
     const alicePeerId = await alice.peerId();
-    const bobPeerId = await bob.peerId();
     const bobMultiAddress = bob.comitNodeNetworkListenAddress();
     const charliePeerId = await charlie.peerId();
     const charlieMultiAddress = charlie.comitNodeNetworkListenAddress();
@@ -21,14 +20,10 @@ declare var global: HarnessGlobal;
             let res = await request(alice.comitNodeHttpApiUrl()).get("/peers");
 
             expect(res.status).to.equal(200);
-            expect(res.body.peers).to.not.containSubset([
-                {
-                    id: bobPeerId,
-                },
-            ]);
+            expect(res.body.peers).to.be.empty;
         });
 
-        it("[Alice] Should be able to make a swap request via HTTP api to Bob using a random peer id and a correct ip address", async () => {
+        it("[Alice] Should be able to make a swap request via HTTP api using a random peer id and Bob's ip address", async () => {
             let res = await request(alice.comitNodeHttpApiUrl())
                 .post("/swaps/rfc003")
                 .send({
@@ -66,27 +61,19 @@ declare var global: HarnessGlobal;
             expect(res.header.location).to.be.a("string");
         });
 
-        it("[Alice] Should see Bob's peer id in her list of peers after sending a swap request to him using his ip address", async () => {
+        it("[Alice] Should not see any peers because the address did not resolve to the given PeerID", async () => {
             await sleep(1000);
             let res = await request(alice.comitNodeHttpApiUrl()).get("/peers");
 
             expect(res.status).to.equal(200);
-            expect(res.body.peers).to.containSubset([
-                {
-                    id: bobPeerId,
-                },
-            ]);
+            expect(res.body.peers).to.be.empty;
         });
 
-        it("[Bob] Should see Alice's peer ID in his list of peers after receiving a swap request from Alice", async () => {
+        it("[Bob] Should not see Alice's PeerID because she dialed to a different PeerID", async () => {
             let res = await request(bob.comitNodeHttpApiUrl()).get("/peers");
 
             expect(res.status).to.equal(200);
-            expect(res.body.peers).to.containSubset([
-                {
-                    id: alicePeerId,
-                },
-            ]);
+            expect(res.body.peers).to.be.empty;
         });
 
         it("[Alice] Should not yet see Charlie's peer id in her list of peers", async () => {
