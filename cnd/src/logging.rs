8c1e9b6e7cf5ad021d7e3591bc2263348a929fcf
Thomas Eizinger <thomas@eizinger.io>
2019-07-31 15:34:41 +1000

    Add test for checking format of line based log message

--- cnd/src/logging.rs
+++ cnd/src/logging.rs
@@ -9,6 +9,19 @@ pub fn set_up_logging(
     #![allow(clippy::print_stdout)] // We cannot use `log` before we have the config file
     println!("Initializing logging with base level {}", base_log_level);
 
+    let (max_level, log) = create_logger(base_log_level, structured, stdout());
+
+    log::set_boxed_logger(log)?;
+    log::set_max_level(max_level);
+
+    Ok(())
+}
+
+fn create_logger<T: Into<fern::Output>>(
+    base_log_level: LevelFilter,
+    structured: bool,
+    target: T,
+) -> (LevelFilter, Box<dyn log::Log>) {
     let formatter = if structured {
         json_formatter
     } else {
@@ -25,8 +38,8 @@ pub fn set_up_logging(
         .level_for("libp2p", LevelFilter::Debug) // the libp2p library
         .level_for("sub-libp2p", LevelFilter::Debug) // the libp2p subsystem in our application
         .level_for("http-api", LevelFilter::Debug) // the http-api of our application
-        .chain(stdout())
-        .apply()
+        .chain(target)
+        .into_log()
 }
 
 fn line_formatter(out: FormatCallback<'_>, message: &Arguments<'_>, record: &Record<'_>) {
@@ -44,3 +57,49 @@ fn line_formatter(out: FormatCallback<'_>, message: &Arguments<'_>, record: &Rec
 }
 
 fn json_formatter(_out: FormatCallback<'_>, _message: &Arguments<'_>, _record: &Record<'_>) {}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::logging::create_logger;
+    use log::{Level, LevelFilter, Record};
+    use spectral::prelude::*;
+    use std::sync::mpsc::channel;
+
+    #[test]
+    fn line_formatter_should_return_a_single_line() {
+        let (sender, receiver) = channel();
+        let (_, log) = create_logger(LevelFilter::Trace, false, sender);
+
+        log.log(
+            &Record::builder()
+                .args(format_args!("Hello {}!", "world"))
+                .level(Level::Debug)
+                .target("test")
+                .line(Some(10))
+                .build(),
+        );
+
+        let messages = receiver.recv().unwrap();
+        let messages = messages
+            .split("\n")
+            .filter(|m| !m.is_empty())
+            .collect::<Vec<_>>();
+
+        assert_that(&messages).has_length(1);
+
+        let pattern = r#"\[[0-9\-\s:\.]+\]\[DEBUG\]\[test:10\] Hello world!"#;
+        let regex = regex::Regex::new(pattern).unwrap();
+        for message in messages {
+            if !regex.is_match(&message) {
+                panic!(
+                    "Log message didn't match expected pattern!\n\n\
+                     Pattern: {}\n\
+                     Message: {}\n",
+                    pattern, message
+                );
+            }
+        }
+    }
+
+}
