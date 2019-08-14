611ee5b8dba27fe6f2c340586e104cf3fa030786
Tobin C. Harding <tobin.harding@coblox.tech>
2019-08-14 15:42:12 +1000

    btsieve: Use directories crate to handle config file parsing
    Currently we hard code `.config` under the users home directory as the
    configuration directory.  This does not correctly handle multiple
    platforms, we can do better than this.
    
    Use `directories` crate to get the configuration directory in the
    correct manner for the host platform.

--- btsieve/src/load_settings.rs
+++ btsieve/src/load_settings.rs
@@ -1,5 +1,6 @@
 use crate::settings::Settings;
 use config::ConfigError;
+use directories::ProjectDirs;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
@@ -13,17 +14,18 @@ pub struct Opt {
 pub fn load_settings(opt: Opt) -> Result<Settings, ConfigError> {
     match opt.config_file {
         Some(file) => parse_config_file(file),
-        None => match directories::UserDirs::new() {
-            None => Err(ConfigError::Message(
-                "Unable to determine user's home directory".to_string(),
-            )),
-            Some(dirs) => {
-                let user_path_components: PathBuf =
-                    [".config", "comit", "btsieve.toml"].iter().collect();
-                let file = Path::join(dirs.home_dir(), user_path_components);
+        None => match ProjectDirs::from("tech", "CoBloX", "btsieve") {
+            // Linux: /home/<user>/.config/btsieve
+            // Windows: C:\Users\<user>\AppData\Roaming\CoBloX\btsieve\config
+            // OSX: /Users/<user>/Library/Preferences/tech.CoBloX.btsieve
+            Some(proj_dirs) => {
+                let file = Path::join(proj_dirs.config_dir(), "btsieve.toml");
                 log::info!("Config file was not provided - looking up config file in default location at: {:?}", file);;
                 parse_config_file(file)
             }
+            None => Err(ConfigError::Message(
+                "Could not generate configuration directory".to_string(),
+            )),
         },
     }
 }
