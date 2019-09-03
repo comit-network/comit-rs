b947d2f360cafe0dd982f0e805cc707501e279a8
tcharding <me@tobin.cc>
2019-09-03 14:49:12 +1000

    Make `directories` usage uniform
    We use the `directories` crate in both btsieve and cnd.  We should use it in a
    uniform manner.  Wrap the usage of `directories` in a function call to improve
    maintainability.  Do the same in both btsieve and cnd.  While we are at it use
    combinators to handle the error return code path instead of using `if let`, its
    cleaner to have the error code indented as a combinator.

--- btsieve/src/load_settings.rs
+++ btsieve/src/load_settings.rs
@@ -19,21 +19,18 @@ pub fn load_settings(opt: Opt) -> Result<Settings, ConfigError> {
         return parse_config_file(file);
     }
 
-    // Linux: /home/<user>/.config/btsieve
-    // Windows: C:\Users\<user>\AppData\Roaming\comit-network\btsieve\config
-    // OSX: /Users/<user>/Library/Preferences/comit-network.btsieve
-    if let Some(proj_dirs) = ProjectDirs::from("", "comit-network", "btsieve") {
-        let file = Path::join(proj_dirs.config_dir(), "btsieve.toml");
-        println!(
-            "Config file was not provided - looking up config file in default location at: {:?}",
-            file
-        );
-        return parse_config_file(file);
-    }
+    let path = config_dir()
+        .map(|dir| Path::join(&dir, "btsieve.toml"))
+        .ok_or_else(|| {
+            ConfigError::Message("Could not generate default configuration path".to_string())
+        })?;
 
-    Err(ConfigError::Message(
-        "Could not generate configuration directory".to_string(),
-    ))
+    log::info!(
+        "Config file was not provided - looking up config file in default location at: {:?}",
+        path
+    );
+
+    parse_config_file(path)
 }
 
 fn parse_config_file(file: PathBuf) -> Result<Settings, ConfigError> {
@@ -45,4 +42,12 @@ fn parse_config_file(file: PathBuf) -> Result<Settings, ConfigError> {
             file
         )))
     }
+}
+
+// Linux: /home/<user>/.config/btsieve/
+// Windows: C:\Users\<user>\AppData\Roaming\comit-network\btsieve\config\
+// OSX: /Users/<user>/Library/Preferences/comit-network.btsieve/
+fn config_dir() -> Option<PathBuf> {
+    ProjectDirs::from("", "comit-network", "btsieve")
+        .map(|proj_dirs| proj_dirs.config_dir().to_path_buf())
 }
