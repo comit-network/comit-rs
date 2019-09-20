713d8851a1933b335ed588ce9bb2be3821e48135
Tobin C. Harding <tobin.harding@coblox.tech>
2019-09-20 20:40:47 +1000

    Remove Compare trait
    All the current Assets implement Ord, we do not need the custom Compare
    trait.

--- cnd/src/swap_protocols/asset.rs
+++ cnd/src/swap_protocols/asset.rs
@@ -20,52 +20,15 @@ pub trait Asset:
     + Hash
     + FromHttpAsset
     + Into<AssetKind>
+    + Ord
 {
-    fn compare_to(&self, other: &Self) -> Compare;
 }
 
-#[derive(PartialEq, Debug)]
-pub enum Compare {
-    GreaterThan,
-    LesserThan,
-    Equal,
-}
+impl Asset for Amount {}
 
-impl Asset for Amount {
-    fn compare_to(&self, other: &Amount) -> Compare {
-        if self < other {
-            Compare::LesserThan
-        } else if self > other {
-            Compare::GreaterThan
-        } else {
-            Compare::Equal
-        }
-    }
-}
+impl Asset for EtherQuantity {}
 
-impl Asset for EtherQuantity {
-    fn compare_to(&self, other: &EtherQuantity) -> Compare {
-        if self < other {
-            Compare::LesserThan
-        } else if self > other {
-            Compare::GreaterThan
-        } else {
-            Compare::Equal
-        }
-    }
-}
-
-impl Asset for Erc20Token {
-    fn compare_to(&self, other: &Erc20Token) -> Compare {
-        if self.quantity < other.quantity {
-            Compare::LesserThan
-        } else if self.quantity > other.quantity {
-            Compare::GreaterThan
-        } else {
-            Compare::Equal
-        }
-    }
-}
+impl Asset for Erc20Token {}
 
 #[derive(Clone, Derivative, PartialEq)]
 #[derivative(Debug = "transparent")]
@@ -99,29 +62,30 @@ mod test {
     use super::*;
     use ethereum_support::{Address, Erc20Quantity, Erc20Token, U256};
     use spectral::*;
+    use std::cmp::Ordering::*;
 
     #[test]
-    fn test_bitcoin_quantity_compare_to() {
+    fn test_bitcoin_quantity_cmp() {
         let quantity_1_btc = Amount::from_btc(1.0).unwrap();
         let quantity_10_btc = Amount::from_btc(10.0).unwrap();
 
-        assert_that(&quantity_1_btc.compare_to(&quantity_10_btc)).is_equal_to(Compare::LesserThan);
-        assert_that(&quantity_1_btc.compare_to(&quantity_1_btc)).is_equal_to(Compare::Equal);
-        assert_that(&quantity_10_btc.compare_to(&quantity_1_btc)).is_equal_to(Compare::GreaterThan);
+        assert_that(&quantity_1_btc.cmp(&quantity_10_btc)).is_equal_to(Less);
+        assert_that(&quantity_1_btc.cmp(&quantity_1_btc)).is_equal_to(Equal);
+        assert_that(&quantity_10_btc.cmp(&quantity_1_btc)).is_equal_to(Greater);
     }
 
     #[test]
-    fn test_ether_quantity_compare_to() {
+    fn test_ether_quantity_cmp() {
         let quantity_1_eth = EtherQuantity::from_eth(1.0);
         let quantity_10_eth = EtherQuantity::from_eth(10.0);
 
-        assert_that(&quantity_1_eth.compare_to(&quantity_10_eth)).is_equal_to(Compare::LesserThan);
-        assert_that(&quantity_1_eth.compare_to(&quantity_1_eth)).is_equal_to(Compare::Equal);
-        assert_that(&quantity_10_eth.compare_to(&quantity_1_eth)).is_equal_to(Compare::GreaterThan);
+        assert_that(&quantity_1_eth.cmp(&quantity_10_eth)).is_equal_to(Less);
+        assert_that(&quantity_1_eth.cmp(&quantity_1_eth)).is_equal_to(Equal);
+        assert_that(&quantity_10_eth.cmp(&quantity_1_eth)).is_equal_to(Greater);
     }
 
     #[test]
-    fn test_erc20_quantity_compare_to() {
+    fn test_erc20_quantity_cmp() {
         let quantity_1_pay = Erc20Token::new(
             "B97048628DB6B661D4C2aA833e95Dbe1A905B280".parse().unwrap(),
             Erc20Quantity(U256::from(1u64)),
@@ -131,20 +95,20 @@ mod test {
             Erc20Quantity(U256::from(10u64)),
         );
 
-        assert_that(&quantity_1_pay.compare_to(&quantity_10_pay)).is_equal_to(Compare::LesserThan);
-        assert_that(&quantity_1_pay.compare_to(&quantity_1_pay)).is_equal_to(Compare::Equal);
-        assert_that(&quantity_10_pay.compare_to(&quantity_1_pay)).is_equal_to(Compare::GreaterThan);
+        assert_that(&quantity_1_pay.cmp(&quantity_10_pay)).is_equal_to(Less);
+        assert_that(&quantity_1_pay.cmp(&quantity_1_pay)).is_equal_to(Equal);
+        assert_that(&quantity_10_pay.cmp(&quantity_1_pay)).is_equal_to(Greater);
     }
 
     #[test]
-    fn test_different_erc20_quantity_compare_to() {
+    fn test_different_erc20_quantity_cmp() {
         let quantity_1_pay =
             Erc20Token::new(Address::repeat_byte(1), Erc20Quantity(U256::from(1u64)));
         let quantity_10_pay =
             Erc20Token::new(Address::repeat_byte(2), Erc20Quantity(U256::from(10u64)));
 
-        assert_that(&quantity_1_pay.compare_to(&quantity_10_pay)).is_equal_to(Compare::LesserThan);
-        assert_that(&quantity_1_pay.compare_to(&quantity_1_pay)).is_equal_to(Compare::Equal);
-        assert_that(&quantity_10_pay.compare_to(&quantity_1_pay)).is_equal_to(Compare::GreaterThan);
+        assert_that(&quantity_1_pay.cmp(&quantity_10_pay)).is_equal_to(Less);
+        assert_that(&quantity_1_pay.cmp(&quantity_1_pay)).is_equal_to(Equal);
+        assert_that(&quantity_10_pay.cmp(&quantity_1_pay)).is_equal_to(Greater);
     }
 }
