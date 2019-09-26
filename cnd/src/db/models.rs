4a4f9a46df1e095c2a00f4c6b190418d8c042bc7
Thomas Eizinger <thomas@eizinger.io>
2019-09-26 22:03:23 +1000

    SwapId no longer needs to be a custom sql type by itself

--- cnd/src/db/models.rs
+++ cnd/src/db/models.rs
@@ -51,8 +51,7 @@ impl InsertableSwap {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, FromSqlRow, AsExpression)]
-#[sql_type = "Text"]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct SwapId(Uuid);
 
 impl FromStr for SwapId {
