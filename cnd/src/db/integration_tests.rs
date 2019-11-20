63c3b3328fc200e8cf23e5dc35268181eca10c8e
Thomas Eizinger <thomas@eizinger.io>
2019-11-20 17:04:37 +1100

    Integrate testing of `determine_types` into db_roundtrip_test macro

--- cnd/src/db/integration_tests.rs
+++ cnd/src/db/integration_tests.rs
@@ -1,5 +1,9 @@
 use crate::{
-    db::{load_swaps::LoadAcceptedSwap, Retrieve, Save, SaveMessage, Sqlite, Swap},
+    db::{
+        load_swaps::LoadAcceptedSwap,
+        swap_types::{DetermineTypes, SwapTypes},
+        AssetKind, LedgerKind, Retrieve, Save, SaveMessage, Sqlite, Swap,
+    },
     quickcheck::Quickcheck,
     swap_protocols::{
         ledger::{Bitcoin, Ethereum},
@@ -10,10 +14,10 @@ use bitcoin::Amount as BitcoinAmount;
 use ethereum_support::{Erc20Token, EtherQuantity};
 
 macro_rules! db_roundtrip_test {
-    ($alpha_ledger:ident, $beta_ledger:ident, $alpha_asset:ident, $beta_asset:ident) => {
+    ($alpha_ledger:ident, $beta_ledger:ident, $alpha_asset:ident, $beta_asset:ident, $expected_swap_types_fn:expr) => {
         paste::item! {
             #[test]
-            #[allow(non_snake_case)]
+            #[allow(non_snake_case, clippy::redundant_closure_call)]
             fn [<roundtrip_test_ $alpha_ledger _ $beta_ledger _ $alpha_asset _ $beta_asset>]() {
                 fn prop(swap: Quickcheck<Swap>,
                         request: Quickcheck<Request<$alpha_ledger, $beta_ledger, $alpha_asset, $beta_asset>>,
@@ -22,6 +26,9 @@ macro_rules! db_roundtrip_test {
 
                     // unpack the swap from the generic newtype
                     let Swap { swap_id, role, counterparty } = swap.0;
+
+                    // construct the expected swap types from the function we get passed in order to enrich it with the role
+                    let expected_swap_types = ($expected_swap_types_fn)(role);
 
                     let db_path = tempfile::Builder::new()
                         .prefix(&swap_id.to_string())
@@ -51,8 +58,14 @@ macro_rules! db_roundtrip_test {
 
                     let loaded_swap = Retrieve::get(&db, &swap_id)?;
                     let (loaded_request, loaded_accept) = db.load_accepted_swap(swap_id)?;
+                    let loaded_swap_types = db.determine_types(&swap_id)?;
 
-                    Ok(saved_request == loaded_request && saved_accept == loaded_accept && loaded_swap == saved_swap)
+                    Ok(
+                        saved_request == loaded_request &&
+                        saved_accept == loaded_accept &&
+                        loaded_swap == saved_swap &&
+                        loaded_swap_types == expected_swap_types
+                    )
                 }
 
                 quickcheck::quickcheck(prop as fn(
@@ -65,7 +78,39 @@ macro_rules! db_roundtrip_test {
     };
 }
 
-db_roundtrip_test!(Bitcoin, Ethereum, BitcoinAmount, EtherQuantity);
-db_roundtrip_test!(Ethereum, Bitcoin, EtherQuantity, BitcoinAmount);
-db_roundtrip_test!(Bitcoin, Ethereum, BitcoinAmount, Erc20Token);
-db_roundtrip_test!(Ethereum, Bitcoin, Erc20Token, BitcoinAmount);
+db_roundtrip_test!(Bitcoin, Ethereum, BitcoinAmount, EtherQuantity, |role| {
+    SwapTypes {
+        alpha_ledger: LedgerKind::Bitcoin,
+        beta_ledger: LedgerKind::Ethereum,
+        alpha_asset: AssetKind::Bitcoin,
+        beta_asset: AssetKind::Ether,
+        role,
+    }
+});
+db_roundtrip_test!(Ethereum, Bitcoin, EtherQuantity, BitcoinAmount, |role| {
+    SwapTypes {
+        alpha_ledger: LedgerKind::Ethereum,
+        beta_ledger: LedgerKind::Bitcoin,
+        alpha_asset: AssetKind::Ether,
+        beta_asset: AssetKind::Bitcoin,
+        role,
+    }
+});
+db_roundtrip_test!(Bitcoin, Ethereum, BitcoinAmount, Erc20Token, |role| {
+    SwapTypes {
+        alpha_ledger: LedgerKind::Bitcoin,
+        beta_ledger: LedgerKind::Ethereum,
+        alpha_asset: AssetKind::Bitcoin,
+        beta_asset: AssetKind::Erc20,
+        role,
+    }
+});
+db_roundtrip_test!(Ethereum, Bitcoin, Erc20Token, BitcoinAmount, |role| {
+    SwapTypes {
+        alpha_ledger: LedgerKind::Ethereum,
+        beta_ledger: LedgerKind::Bitcoin,
+        alpha_asset: AssetKind::Erc20,
+        beta_asset: AssetKind::Bitcoin,
+        role,
+    }
+});
