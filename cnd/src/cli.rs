e0efc9987645a8b190d8c8fae2f4acf528168fa3
Tobin C. Harding <tobin.harding@coblox.tech>
2019-11-29 14:36:03 +1100

    Remove period from help output

--- cnd/src/cli.rs
+++ cnd/src/cli.rs
@@ -3,11 +3,11 @@ use std::path::PathBuf;
 #[derive(structopt::StructOpt, Debug)]
 #[structopt(name = "COMIT network daemon")]
 pub struct Options {
-    /// Path to configuration file.
+    /// Path to configuration file
     #[structopt(short = "c", long = "config", parse(from_os_str))]
     pub config_file: Option<PathBuf>,
 
-    /// Dump the current configuration and exit.
+    /// Dump the current configuration and exit
     #[structopt(long = "dump-config")]
     pub dump_config: bool,
 }
