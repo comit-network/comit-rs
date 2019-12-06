87cacce4dad17b061bbc41a93046e5b3636fd030
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-12-06 14:41:05 +1100

    Align sanity tests names with what they now do

--- api_tests/dry/sanity.ts
+++ api_tests/dry/sanity.ts
@@ -33,7 +33,7 @@ setTimeout(async function() {
             expect(body.entities).to.have.lengthOf(0);
         });
 
-        it("[Alice] Returns 400 Swap not supported for an unsupported combination of parameters", async () => {
+        it("[Alice] Returns 400 invalid body for an unsupported combination of parameters", async () => {
             const res = await request(alice.cndHttpApiUrl())
                 .post("/swaps/rfc003")
                 .send({
@@ -66,7 +66,7 @@ setTimeout(async function() {
             expect(res.body.title).to.equal("Invalid body.");
         });
 
-        it("[Alice] Returns 400 bad request for malformed requests", async () => {
+        it("[Alice] Returns 400 invalid body for malformed requests", async () => {
             const res = await request(alice.cndHttpApiUrl())
                 .post("/swaps/rfc003")
                 .send({
