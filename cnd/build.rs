2b539420858e7ec44eee1ff8f0fbda13eb08521f
Tobin C. Harding <tobin.harding@coblox.tech>
2019-12-17 10:25:38 +1100

    Do not fail build if `git` command not found

--- cnd/build.rs
+++ cnd/build.rs
@@ -1,11 +1,13 @@
 use std::process::Command;
 fn main() {
-    let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .expect("failed to get commit hash for tip of branch");
+    let unknown = String::from("************");
 
-    let git_hash = String::from_utf8(output.stdout).expect("failed to convert hash");
+    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output();
+
+    let git_hash = match output {
+        Ok(output) => String::from_utf8(output.stdout).unwrap_or(unknown),
+        Err(_) => unknown,
+    };
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
