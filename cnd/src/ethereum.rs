d4905fc15d7761cf9be9143165460df140db4177
Thomas Eizinger <thomas@eizinger.io>
2020-01-14 13:44:47 +1100

    Replace web3 with reqwest-based implementation

--- cnd/src/ethereum.rs
+++ cnd/src/ethereum.rs
@@ -2,14 +2,10 @@
 #![forbid(unsafe_code)]
 
 pub use self::{contract_address::*, u256_ext::*};
-pub use ::web3::types::{
+pub use web3::types::{
     Address, Block, BlockId, BlockNumber, Bytes, Log, Transaction, TransactionReceipt,
     TransactionRequest, H160, H2048, H256, U128, U256,
 };
-
-pub mod web3 {
-    pub use ::web3::{transports, Error, Web3};
-}
 
 mod contract_address;
 pub(crate) mod u256_ext;
