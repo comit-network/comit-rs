548cf6b9104702f362a7637851189e6a6c90964a
Franck Royer <franck@coblox.tech>
2020-02-10 14:11:42 +1100

    test: Use meaningful name

--- api_tests/lib_sdk/utils.ts
+++ api_tests/lib_sdk/utils.ts
@@ -1,6 +1,6 @@
-export async function sleep(time: number) {
+export async function sleep(ms: number) {
     return new Promise(res => {
-        setTimeout(res, time);
+        setTimeout(res, ms);
     });
 }
 
