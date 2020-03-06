c47fde55e4d18a675403d2bcd83f1f6ea626dda7
Thomas Eizinger <thomas@eizinger.io>
2020-03-06 18:22:40 +1100

    Use jsonrpc client to replace web3

--- cnd/tests/ethereum_transaction_matching_smoke_test.rs
+++ cnd/tests/ethereum_transaction_matching_smoke_test.rs
@@ -1,17 +1,12 @@
 use chrono::offset::Utc;
 use cnd::{
     btsieve::ethereum::{matching_transaction_and_receipt, Web3Connector},
-    ethereum::U256,
+    ethereum::{Address, U256},
+    jsonrpc,
 };
-use futures_core::compat::Future01CompatExt;
 use reqwest::Url;
 use std::time::Duration;
 use testcontainers::*;
-use web3::{
-    transports::{EventLoopHandle, Http},
-    types::TransactionRequest,
-    Web3,
-};
 
 /// A very basic e2e test that verifies that we glued all our code together
 /// correctly for ethereum transaction pattern matching.
@@ -25,23 +20,13 @@ async fn ethereum_transaction_matching_smoke_test() {
     let container = cli.run(images::parity_parity::ParityEthereum::default());
     let start_of_swap = Utc::now().naive_local();
 
-    let (_handle, client) = new_web3_client(&container);
+    let url = connection_url(&container).unwrap();
 
-    let mut url = Url::parse("http://localhost").expect("failed to parse static URL");
-    #[allow(clippy::cast_possible_truncation)]
-    url.set_port(Some(
-        container
-            .get_host_port(8545)
-            .expect("failed to get host port") as u16,
-    ))
-    .unwrap();
-
+    let client = jsonrpc::Client::new(url.clone());
     let connector = Web3Connector::new(url);
 
-    let accounts = client
-        .eth()
-        .accounts()
-        .compat()
+    let accounts: Vec<Address> = client
+        .send(jsonrpc::Request::new("eth_accounts", Vec::<u32>::new()))
         .await
         .expect("failed to get accounts");
 
@@ -50,23 +35,17 @@ async fn ethereum_transaction_matching_smoke_test() {
     let send_money_to_address = async {
         tokio::time::delay_for(Duration::from_secs(2)).await;
         client
-            .personal()
-            .send_transaction(
-                TransactionRequest {
+            .send(jsonrpc::Request::new("personal_sendTransaction", vec![
+                jsonrpc::serialize(TransactionRequest {
                     from: "00a329c0648769a73afac7f9381e08fb43dbea72"
                         .parse()
                         .expect("failed to parse static string"),
-                    to: Some(target_address),
-                    gas: None,
-                    gas_price: None,
-                    value: Some(U256::from(1_000_000_000u64)),
-                    data: None,
-                    nonce: None,
-                    condition: None,
-                },
-                "",
-            )
-            .compat()
+                    to: target_address,
+                    value: U256::from(1_000_000_000u64),
+                })
+                .unwrap(),
+                jsonrpc::serialize("").unwrap(),
+            ]))
             .await
             .expect("failed to send transaction")
     };
@@ -87,7 +66,7 @@ async fn ethereum_transaction_matching_smoke_test() {
     assert_eq!(matched_transaction.hash, transaction)
 }
 
-pub fn new_web3_client<D, E>(container: &Container<'_, D, E>) -> (EventLoopHandle, Web3<Http>)
+fn connection_url<D, E>(container: &Container<'_, D, E>) -> anyhow::Result<reqwest::Url>
 where
     D: Docker,
     E: Image,
@@ -95,8 +74,14 @@ where
     let port = container.get_host_port(8545).unwrap();
     let endpoint = format!("http://localhost:{}", port);
 
-    let (event_loop, transport) = Http::new(&endpoint).unwrap();
-    let web3 = Web3::new(transport);
+    let url = Url::parse(&endpoint)?;
 
-    (event_loop, web3)
+    Ok(url)
+}
+
+#[derive(Clone, Debug, PartialEq, serde::Serialize)]
+struct TransactionRequest {
+    pub from: Address,
+    pub to: Address,
+    pub value: U256,
 }
