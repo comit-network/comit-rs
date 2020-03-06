89a632c1b59adcdedd5eee15b3768dce89b6c611
Thomas Eizinger <thomas@eizinger.io>
2020-03-06 15:07:00 +1100

    Replace custom hex_serde module with serde-hex crate

--- cnd/src/ethereum/mod.rs
+++ cnd/src/ethereum/mod.rs
@@ -1,24 +1,16 @@
 #![warn(unused_extern_crates, missing_debug_implementations, rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
-pub use primitive_types::{H160, H256, U128, U256};
-
 pub use ethbloom::{Bloom as H2048, Input};
-
-use hex::{FromHex, ToHex};
-use serde::{
-    de::{Error, Visitor},
-    Deserialize, Deserializer, Serialize, Serializer,
-};
-use std::fmt;
-
-mod hex_serde;
+pub use primitive_types::{H160, H256, U128, U256};
+use serde::{Deserialize, Serialize, Serializer};
+use serde_hex::{SerHex, SerHexSeq, StrictPfx};
 
 pub type Index = U128;
 pub type Address = H160;
 
 #[derive(Debug, Default, Copy, Clone, PartialEq, Deserialize)]
-pub struct H64(#[serde(with = "hex_serde")] [u8; 8]);
+pub struct H64(#[serde(with = "SerHex::<StrictPfx>")] [u8; 8]);
 
 /// "Receipt" of an executed transaction: details of its execution.
 #[derive(Debug, Default, Clone, PartialEq, Deserialize)]
@@ -67,7 +59,7 @@ impl IsStatusOk for TransactionReceipt {
 }
 
 /// Description of a Transaction, pending or in the chain.
-#[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
+#[derive(Debug, Default, Clone, PartialEq, Deserialize)]
 pub struct Transaction {
     /// Hash
     pub hash: H256,
@@ -199,62 +191,12 @@ impl Serialize for BlockNumber {
 }
 
 /// Raw bytes wrapper
-#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
-pub struct Bytes(pub Vec<u8>);
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Bytes(#[serde(with = "SerHexSeq::<StrictPfx>")] pub Vec<u8>);
 
 impl<T: Into<Vec<u8>>> From<T> for Bytes {
     fn from(data: T) -> Self {
         Bytes(data.into())
-    }
-}
-
-impl Serialize for Bytes {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut serialized = "0x".to_owned();
-        serialized.push_str(self.0.encode_hex::<String>().as_ref());
-        serializer.serialize_str(serialized.as_ref())
-    }
-}
-
-impl<'a> Deserialize<'a> for Bytes {
-    fn deserialize<D>(deserializer: D) -> Result<Bytes, D::Error>
-    where
-        D: Deserializer<'a>,
-    {
-        deserializer.deserialize_identifier(BytesVisitor)
-    }
-}
-
-struct BytesVisitor;
-
-impl<'a> Visitor<'a> for BytesVisitor {
-    type Value = Bytes;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(formatter, "a 0x-prefixed hex-encoded vector of bytes")
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        if value.len() >= 2 && &value[0..2] == "0x" && value.len() & 1 == 0 {
-            Ok(Bytes(
-                FromHex::from_hex(&value[2..]).map_err(|_| Error::custom("invalid hex"))?,
-            ))
-        } else {
-            Err(Error::custom("invalid format"))
-        }
-    }
-
-    fn visit_string<E>(self, value: String) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        self.visit_str(value.as_ref())
     }
 }
 
