2ebcb64ad483843682651bbf3892a4664ff82494
Franck Royer <franck@coblox.tech>
2020-03-11 20:27:28 +1100

    Put separator in a const

--- digest/src/lib.rs
+++ digest/src/lib.rs
@@ -1,5 +1,7 @@
 use multihash::Multihash;
 
+const SEPARATOR: &[u8; 1] = b":";
+
 pub fn digest(bytes: &[u8]) -> Multihash {
     // Time the tests and take fastest hash?
     multihash::Sha3_256::digest(bytes)
@@ -16,7 +18,7 @@ pub trait DigestField {
 impl DigestField for String {
     fn digest_field(self, field_name: String) -> Multihash {
         let mut bytes = field_name.into_bytes();
-        let mut separator = b": ".to_vec();
+        let mut separator = SEPARATOR.to_vec();
         bytes.append(&mut separator);
         let mut value = self.into_bytes();
         bytes.append(&mut value);
@@ -28,7 +30,7 @@ impl DigestField for String {
 impl DigestField for Vec<u8> {
     fn digest_field(mut self, field_name: String) -> Multihash {
         let mut bytes = field_name.into_bytes();
-        let mut separator = b": ".to_vec();
+        let mut separator = SEPARATOR.to_vec();
         bytes.append(&mut separator);
         bytes.append(&mut self);
 
