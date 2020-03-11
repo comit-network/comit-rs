acecd0fbeb9a748cd8b55ffb5403fa080c953b79
Franck Royer <franck@coblox.tech>
2020-03-11 15:48:26 +1100

    Implement for new type

--- cnd/src/swap_digest.rs
+++ cnd/src/swap_digest.rs
@@ -12,6 +12,14 @@ impl Digest for String {
     }
 }
 
+struct NewType(String);
+
+impl Digest for NewType {
+    fn digest(&self) -> Multihash {
+        self.0.digest()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +30,29 @@ mod tests {
         let str2 = String::from("simple string");
 
         assert_eq!(str1.digest(), str2.digest())
+    }
+
+    #[test]
+    fn given_differemt_strings_return_different_multihash() {
+        let str1 = String::from("simple string");
+        let str2 = String::from("longer string.");
+
+        assert_ne!(str1.digest(), str2.digest())
+    }
+
+    #[test]
+    fn given_same_newtypes_return_same_multihash() {
+        let new_type1 = NewType("simple string".into());
+        let new_type2 = NewType("simple string".into());
+
+        assert_eq!(new_type1.digest(), new_type2.digest())
+    }
+
+    #[test]
+    fn given_different_newtypes_return_different_multihash() {
+        let new_type1 = NewType("simple string".into());
+        let new_type2 = NewType("longer string.".into());
+
+        assert_ne!(new_type1.digest(), new_type2.digest())
     }
 }
