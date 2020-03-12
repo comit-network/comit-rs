ac64ce6d2bd3af217e61e6ca9052ca1587a5fb72
Franck Royer <franck@coblox.tech>
2020-03-12 17:08:46 +1100

    Remove separator

--- digest/src/lib.rs
+++ digest/src/lib.rs
@@ -4,8 +4,6 @@ pub use hex;
 pub use multihash;
 use multihash::Multihash;
 
-const SEPARATOR: u8 = 0u8;
-
 pub fn digest(bytes: &[u8]) -> Multihash {
     multihash::Sha3_256::digest(bytes)
 }
@@ -21,8 +19,6 @@ pub trait FieldDigest {
 impl FieldDigest for String {
     fn field_digest(self, suffix: Vec<u8>) -> Multihash {
         let mut bytes = suffix;
-        let mut separator = [SEPARATOR].to_vec();
-        bytes.append(&mut separator);
         // String::into_bytes return the bytes for UTF-8 encoding
         let mut value = self.into_bytes();
         bytes.append(&mut value);
@@ -34,8 +30,6 @@ impl FieldDigest for String {
 impl FieldDigest for Vec<u8> {
     fn field_digest(mut self, suffix: Vec<u8>) -> Multihash {
         let mut bytes = suffix;
-        let mut separator = [SEPARATOR].to_vec();
-        bytes.append(&mut separator);
         bytes.append(&mut self);
 
         digest(&self)
