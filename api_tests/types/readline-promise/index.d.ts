539674ec70f426ed1c63d91482737795bf697eef
Thomas Eizinger <thomas@eizinger.io>
2020-03-17 16:17:57 +1100

    Remove unnecessary dependencies from the e2e tests

--- api_tests/types/readline-promise/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "readline-promise" {
-    interface ReadLine {
-        questionAsync(value: string): Promise<string>;
-    }
-
-    function createInterface(options: any): ReadLine;
-}
