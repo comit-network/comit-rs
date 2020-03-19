ba38fca9c73e6e4da1d6814b1922ffe8eb00c095
Thomas Eizinger <thomas@eizinger.io>
2020-03-19 17:07:48 +1100

    fixup! Only start one instance of Parity and Bitcoind ever

--- api_tests/src/environment/cleanup.ts
+++ api_tests/src/environment/cleanup.ts
@@ -3,7 +3,7 @@ import { EnvGlobal } from "./prepare";
 
 declare var global: EnvGlobal;
 
-module.exports = async () => {
+export default async () => {
     // delete the locks dir folder to make sure we don't leave old configuration files behind
     await rimrafAsync(global.locksDir);
 };
