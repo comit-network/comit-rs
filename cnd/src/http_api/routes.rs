111084e388fe7771b480953467b189cac6633ec6
Franck Royer <franck@coblox.tech>
2020-05-12 13:36:32 +1000

    Explain lightning invoice expiry

--- cnd/src/http_api/routes.rs
+++ cnd/src/http_api/routes.rs
@@ -20,6 +20,7 @@ use crate::{
         DeployAction, Facade, FundAction, Herc20HalightBitcoinCreateSwapParams, InitAction,
         LocalSwapId, RedeemAction, RefundAction, Role,
     },
+    timestamp::RelativeTime,
     transaction,
 };
 use blockchain_contracts::ethereum::rfc003::{ether_htlc::EtherHtlc, Erc20Htlc};
@@ -27,6 +28,92 @@ use http_api_problem::HttpApiProblem;
 use serde::Serialize;
 use std::collections::HashMap;
 use warp::{http, http::StatusCode, Rejection, Reply};
+
+/// The lightning invoice expiry is used to tell the receiving lnd
+/// until when should the payment of this invoice can be accepted.
+///
+/// If a payer tries to pay an expired invoice, lnd will automatically
+/// reject the payment.
+///
+/// In the case of han-ether-halight, there are 3 expiries to take in
+/// account:
+/// 1. alpha expiry: The absolute time from when ether can be refunded to
+/// Alice
+/// 2. cltv or beta expiry: The relative time from when Bob can go on chain
+/// to get his lightning bitcoin back. This is relative to when the
+/// lightning htlc are sent to the blockchain as it uses
+/// OP_CHECKSEQUENCEVERIFY.
+/// 3. invoice expiry: The relative time from when Alice's lnd will not
+/// accept a lightning payment from Bob. This is relative to when the hold
+/// invoice is added by Alice to her lnd.
+///
+/// In terms of security, the beta expiry should expire before alpha expiry
+/// with enough margin to ensure that Bob can refund his bitcoin (by going
+/// on chain) before Alice can attempt to refund her ether.
+///
+/// So it should go:
+/// cltv/beta expiry < min time to refund bitcoin < alpha expiry
+///
+/// The cltv expiry is relative so it means that once the values are agreed,
+/// several actions needs to happen before we can now the actual (absolute)
+/// beta expiry:
+/// 1. Alice adds lnd invoice
+/// 2. Bob send lnd payment
+/// 3. Bob force closes the used lightning channel by broadcasting the
+/// lightning htlcs.
+/// 4. The lightning htlcs are mined in a block.
+/// Once step 4 is done, then it is possible to know when bob can actually
+/// refund his bitcoin.
+///
+/// Which means the following actions matter to keep the swap atomic:
+/// 1. Alice and Bob agree on cltv and alpha expiry
+///   > Alice control
+/// 2. Alice adds lnd invoice
+///   > Invoice expiry
+/// 3. Bob sends lightning payment
+///   > Bob control
+/// 4. Bob force closes lightning channel
+///   > Bitcoin blockchain
+/// 5. Lightning htlcs are mined
+///   > cltv expiry
+/// 6. Lightning htlcs are expired
+///   > Bob control/Immediate
+/// 7. Bob sends Bitcoin refund transactions
+///   > Bitcoin blockchain
+/// 8. Bob's Bitcoin refund transactions are securely confirmed
+///   > Alpha expiry
+/// 9. Ether htlc is expired
+///
+/// If we only extract the waiting periods:
+/// 0 -> Alice
+///     -> invoice expiry
+///         -> Bob
+///             -> Bitcoin
+///                 -> cltv expiry
+///                     -> Bitcoin
+///                         -> Alpha expiry
+///
+/// Note that the invoice expiry here protects Bob from locking its bitcoins
+/// late in process, at a time where he tried to back out, it would not have
+/// time to refund before Alice can redeem and refund.
+///
+/// We are currently setting the smallest expiry for Ethereum<>Bitcoin
+/// onchain swaps to 12 hours but we do not recommend from Bob should
+/// refrain to lock their asset. The invoice expiry value should be set to
+/// this recommendation (that we currently do not provide).
+///
+/// Do not that Bob should not lock their funds immediately after Alice has
+/// locked hers either. Bob should wait long enough to ensure that Alice's
+/// asset cannot be sent to a different address by the way of a chain
+/// re-org. According to various sources, it seems that 12 confirmations on
+/// Ethereum (3min24s) is the equivalent of the 6 Bitcoin confirmations.
+///
+/// So Bob should probably wait at least 3 minutes after Alice locks her
+/// Ether but not so long as to risk getting close to the absolute alpha
+/// expiry.
+///
+/// Hence, 1 hour expiry seems to be a fair bet.
+const INVOICE_EXPIRY_SECS: RelativeTime = RelativeTime::new(3600);
 
 pub fn into_rejection(problem: HttpApiProblem) -> Rejection {
     warp::reject::custom(problem)
@@ -558,7 +645,7 @@ impl InitAction for AliceHerc20HalightBitcoinState {
             halight::State::None => {
                 let amount = self.finalized_swap.beta_asset;
                 let secret_hash = self.finalized_swap.secret_hash;
-                let expiry = 3600.into();
+                let expiry = INVOICE_EXPIRY_SECS;
                 let cltv_expiry = self.finalized_swap.beta_expiry;
                 let chain = Chain::Bitcoin;
                 let network = bitcoin::Network::Regtest;
