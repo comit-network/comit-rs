4fa90287fd514688ef68d126d40250f76b74d2a7
Franck Royer <franck@coblox.tech>
2020-06-10 13:26:17 +1000

    Add state

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,16 @@ impl Order {
 #[derive(Copy, Clone, Default)]
 struct Peer;
 
-fn should_proceed(_order: Order) -> bool {
-    true
+#[derive(Default)]
+struct State;
+
+impl State {
+    fn proceed(&self, _order: Order) -> Result<(), ()> {
+        Ok(())
+    }
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -24,25 +31,29 @@ mod tests {
 
     #[test]
     fn given_a_taken_order_return_yes_proceed() {
+        let state = State::default();
+
         let order = Order::default();
 
-        let proceed = should_proceed(order);
+        let proceed = state.proceed(order);
 
-        assert!(proceed);
+        assert!(proceed.is_ok());
     }
 
     #[test]
     fn given_two_orders_from_same_peer_dont_proceed_second_one() {
+        let state = State::default();
+
         let peer = Peer::default();
 
         let order_1 = Order::new(peer);
         let order_2 = Order::new(peer);
 
-        let proceed_1 = should_proceed(order_1);
+        let proceed_1 = state.proceed(order_1);
 
-        let proceed_2 = should_proceed(order_2);
+        let proceed_2 = state.proceed(order_2);
 
-        assert!(proceed_1);
-        assert!(!proceed_2);
+        assert!(proceed_1.is_ok());
+        assert!(proceed_2.is_err());
     }
 }
