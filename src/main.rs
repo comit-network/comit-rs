27629a7579f66c4c0be7672b7d8f998aed4c21eb
Franck Royer <franck@coblox.tech>
2020-06-10 13:23:13 +1000

    test: Do not execute 2 order from same peer at the same time

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ fn main() {
 #[derive(Default)]
 struct Order;
 
+impl Order {
+    pub fn new(peer: Peer) -> Order {
+        Order
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+struct Peer;
+
 fn should_proceed(_order: Order) -> bool {
     true
 }
@@ -20,5 +29,20 @@ mod tests {
         let proceed = should_proceed(order);
 
         assert!(proceed);
+    }
+
+    #[test]
+    fn given_two_orders_from_same_peer_dont_proceed_second_one() {
+        let peer = Peer::default();
+
+        let order_1 = Order::new(peer);
+        let order_2 = Order::new(peer);
+
+        let proceed_1 = should_proceed(order_1);
+
+        let proceed_2 = should_proceed(order_2);
+
+        assert!(proceed_1);
+        assert!(!proceed_2);
     }
 }
