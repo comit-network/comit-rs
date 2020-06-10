ff7e347ef0f49fb5ad33c727a73875578f344db1
Franck Royer <franck@coblox.tech>
2020-06-10 14:06:03 +1000

    Order to be published sells available balance

--- src/publish.rs
+++ src/publish.rs
@@ -2,9 +2,9 @@ struct Order {
     pub sell_amount: u32,
 }
 
-fn new_order(balance: u32) -> Order {
+fn new_order(balance: u32, locked_funds: u32) -> Order {
     Order {
-        sell_amount: balance,
+        sell_amount: balance - locked_funds,
     }
 }
 
@@ -16,8 +16,19 @@ mod tests {
     fn given_a_balance_return_order_selling_full_balance() {
         let balance = 10u32;
 
-        let order = new_order(balance);
+        let order = new_order(balance, 0);
 
-        assert_eq!(order.sell_amount, balance);
+        assert_eq!(order.sell_amount, 10u32);
+    }
+
+    #[test]
+    fn given_a_balance_and_locked_funds_return_order_selling_available_balance() {
+        let balance = 10u32;
+
+        let locked_funds = 2u32;
+
+        let order = new_order(balance, locked_funds);
+
+        assert_eq!(order.sell_amount, 8u32);
     }
 }
