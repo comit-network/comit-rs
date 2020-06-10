76280091a480540d87c39965ea6c4ec1d5a92b76
Franck Royer <franck@coblox.tech>
2020-06-10 13:17:15 +1000

    Always proceed with an order

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Default)]
+struct Order;
+
+fn should_proceed(_order: Order) -> bool {
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn given_a_taken_order_return_yes_proceed() {
+        let order = Order::default();
+
+        let proceed = should_proceed(order);
+
+        assert!(proceed);
+    }
+}
