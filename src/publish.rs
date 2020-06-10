d1d8711df4420823ec7945c79e29fe81ae917e41
Franck Royer <franck@coblox.tech>
2020-06-10 14:12:50 +1000

    Balance should be passed via a wallet struct that implements Balance trait

--- src/publish.rs
+++ src/publish.rs
@@ -2,16 +2,21 @@ pub trait LockedFunds {
     fn locked_funds(&self) -> u32;
 }
 
+pub trait Balance {
+    fn balance(&self) -> u32;
+}
+
 struct Order {
     pub sell_amount: u32,
 }
 
-fn new_order<B>(balance: u32, book: B) -> Order
+fn new_order<W, B>(wallet: W, book: B) -> Order
 where
+    W: Balance,
     B: LockedFunds,
 {
     Order {
-        sell_amount: balance - book.locked_funds(),
+        sell_amount: wallet.balance() - book.locked_funds(),
     }
 }
 
@@ -21,6 +26,22 @@ mod tests {
 
     struct Book {
         locked_funds: u32,
+    }
+
+    struct Wallet {
+        balance: u32,
+    }
+
+    impl Wallet {
+        fn new(balance: u32) -> Wallet {
+            Wallet { balance }
+        }
+    }
+
+    impl Balance for Wallet {
+        fn balance(&self) -> u32 {
+            self.balance
+        }
     }
 
     impl Book {
@@ -37,22 +58,22 @@ mod tests {
 
     #[test]
     fn given_a_balance_return_order_selling_full_balance() {
-        let balance = 10u32;
+        let wallet = Wallet::new(10);
 
         let book = Book::new(0);
 
-        let order = new_order(balance, book);
+        let order = new_order(wallet, book);
 
         assert_eq!(order.sell_amount, 10u32);
     }
 
     #[test]
     fn given_a_balance_and_locked_funds_return_order_selling_available_balance() {
-        let balance = 10u32;
+        let wallet = Wallet::new(10);
 
         let book = Book::new(2);
 
-        let order = new_order(balance, book);
+        let order = new_order(wallet, book);
 
         assert_eq!(order.sell_amount, 8u32);
     }
