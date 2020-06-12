5df3e07cb1a9803289807a9682ea8cee9356453f
Franck Royer <franck@coblox.tech>
2020-06-12 15:50:59 +1000

    Fixing clippy for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+#![warn(
+    unused_extern_crates,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    rust_2018_idioms,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss,
+    clippy::fallible_impl_from,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_wrap,
+    clippy::dbg_macro
+)]
+#![allow(dead_code)] // To be removed further down the line
+#![forbid(unsafe_code)]
+
 pub mod swap;
 
 #[cfg(all(test, feature = "test-docker"))]
@@ -10,6 +25,7 @@ pub mod ledgers {
         Container, Docker, Image,
     };
 
+    #[derive(Debug)]
     pub struct BitcoinBlockchain<'c> {
         _container: Container<'c, clients::Cli, BitcoinCore>,
         pub node_url: Url,
@@ -36,6 +52,7 @@ pub mod ledgers {
         }
     }
 
+    #[derive(Debug)]
     pub struct EthereumBlockchain<'c> {
         _container: Container<'c, clients::Cli, GenericImage>,
         pub node_url: Url,
