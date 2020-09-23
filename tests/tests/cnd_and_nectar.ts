b2dc9a192946d7997d7fbe4af9906d9f87fc8540
Thomas Eizinger <thomas@eizinger.io>
2020-09-23 17:33:59 +1000

    Add test where cnd sells and nectar buys Bitcoin

--- tests/tests/cnd_and_nectar.ts
+++ tests/tests/cnd_and_nectar.ts
@@ -33,3 +33,29 @@ test(
         });
     })
 );
+
+test(
+    "given_cnd_and_nectar_when_cnd_publishes_a_matching_sell_order_then_successful_swap",
+    startConnectedCndAndNectar(async ({ alice, bob }) => {
+        await bob.saveBalancesBeforeSwap();
+        await alice.pollCndUntil<MarketEntity>(
+            "/markets/BTC-DAI",
+            (market) => market.entities.length > 0
+        );
+
+        await alice.makeBtcDaiOrder(Position.Sell, "0.23391812", "8550"); // This matches what nectar publishes.
+        await alice.waitForSwap();
+
+        await alice.assertAndExecuteNextAction("fund");
+        await alice.assertAndExecuteNextAction("redeem");
+
+        // Wait until the wallets sees the new balance.
+        await sleep(2000);
+
+        await alice.assertBalancesAfterSwap();
+        await bob.assertBalancesChangedBy({
+            bitcoin: 23391812n - 5700n, // nectar receives order quantity but pays the redeem fee
+            dai: -1_999_999_926_000_000_000_000n, // = 0.23391812 * 8550 * 10^18
+        });
+    })
+);
