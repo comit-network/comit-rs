80844094e9e104839b233f5504c63fa8f76e7444
Thomas Eizinger <thomas@eizinger.io>
2020-09-25 16:12:41 +1000

    Remove timeout from waitForSwap
    We've recently added a timeout to pollCndUntil, hence this one is
    obsolete.

--- tests/src/actors/cnd_actor.ts
+++ tests/src/actors/cnd_actor.ts
@@ -553,15 +553,9 @@ export class CndActor
     }
 
     public async waitForSwap(): Promise<void> {
-        const poller = this.pollCndUntil<Entity>(
+        const response = await this.pollCndUntil<Entity>(
             "/swaps",
             (body) => body.entities.length > 0
-        );
-
-        const response = await pTimeout(
-            poller,
-            10_000,
-            "no swap appeared after 10 seconds"
         );
 
         this.swap = new Swap(
