d249461f0dacea4d65fe37e73d54735a2ec532b6
Thomas Eizinger <thomas@eizinger.io>
2020-09-25 16:01:28 +1000

    Remove tests already covered elsewhere
    hbit-herc20 and herc20-hbit happy path tests are covered in the
    BTC/DAI buy-sell tests.

--- tests/tests/hbit_herc20.ts
+++ tests/tests/hbit_herc20.ts
@@ -9,30 +9,6 @@ import { startAliceAndBob } from "../src/actor_test";
 
 describe("hbit-herc20", () => {
     it(
-        "hbit-herc20-alice-redeems-bob-redeems",
-        startAliceAndBob(async ([alice, bob]) => {
-            const bodies = (await SwapFactory.newSwap(alice, bob)).hbitHerc20;
-
-            await alice.createHbitHerc20Swap(bodies.alice);
-            await bob.createHbitHerc20Swap(bodies.bob);
-
-            await alice.assertAndExecuteNextAction("fund");
-
-            await bob.assertAndExecuteNextAction("deploy");
-            await bob.assertAndExecuteNextAction("fund");
-
-            await alice.assertAndExecuteNextAction("redeem");
-            await bob.assertAndExecuteNextAction("redeem");
-
-            // Wait until the wallet sees the new balance.
-            await sleep(2000);
-
-            await alice.assertBalancesAfterSwap();
-            await bob.assertBalancesAfterSwap();
-        })
-    );
-
-    it(
         "hbit-herc20-bob-refunds-alice-refunds",
         startAliceAndBob(async ([alice, bob]) => {
             const bodies = (
